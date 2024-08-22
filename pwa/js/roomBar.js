a3b60e96a53d042dfd52636959817dde413e563b
rleed <rleed@satoshidnc.com>
2024-08-21 21:21:19 -0400

    dev

--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -4,6 +4,11 @@ export const roomBar = v = new fg.SliceView(null, 'tl', 50)
 v.name = Object.keys({roomBar}).pop()
 v.bgColor = [1,0,0,1]
 v.fontColor = [1,1,1,1]
+v.white = [1,1,1,1]
+v.yellow = [1,0.8,0,1]
+v.red = [1,0,0,1]
+v.blue = [0,0,1,1]
+v.shirtColor = [v.white, v.yellow, v.red, v.blue]
 v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
@@ -13,8 +18,8 @@ v.renderFunc = function() {
     mat4.identity(mat)
     console.log(str, i)
     const x = (v.sw - nybbleFont.calcWidth(str))/2
-    const y = (v.sh)/2 + i*8
-    nybbleFont.draw(x,y, str, v.fontColor, v.mat, mat)
+    const y = (v.sh)/2
+    nybbleFont.draw(x,y + i*8, str, v.shirtColor[i], v.mat, mat)
   })
 }
 
