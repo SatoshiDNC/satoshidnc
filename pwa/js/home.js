7571bc9bf488b77f6a42116a857420cfd1e1ef4d
rleed <rleed@satoshidnc.com>
2024-08-21 20:57:24 -0400

    dev

--- pwa/js/home.js
+++ pwa/js/home.js
@@ -1,6 +1,6 @@
 import { roomBar } from '/roomBar.js'
 
-const homeView = v = new fg.View(null)
+export const homeView = v = new fg.View(null)
 v.name = Object.keys({homeView}).pop()
 v.designSize = 640*400
 v.splashMode = 0
@@ -62,16 +62,16 @@ v.renderFunc = function() {
   // this.setRenderFlag(true)
 }
 
-const homeSend = v = new fg.SliceView(null, 'br', .125)
+export const homeSend = v = new fg.SliceView(null, 'br', .125)
 v.name = Object.keys({homeSend}).pop()
 v.prop = true
 v.a = sendBar; sendBar.parent = v
 v.b = homeView; homeView.parent = v
 
-const homeRoom = v = new fg.SliceView(null, 'tl', .125)
+export const homeRoom = v = new fg.SliceView(null, 'tl', .125)
 v.name = Object.keys({homeRoom}).pop()
 v.prop = true
 v.a = roomBar; roomBar.parent = v
 v.b = homeSend; homeSend.parent = v
 
-const homeRoot = homeRoom
+export const homeRoot = homeRoom
