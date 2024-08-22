4adbb99a522cd0a9af9fcebd1dcea939ee346532
rleed <rleed@satoshidnc.com>
2024-08-21 22:27:28 -0400

    dev

--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -16,7 +16,7 @@ v.renderFunc = function() {
   gl.clear(gl.COLOR_BUFFER_BIT)
   const mat = mat4.create()
   mat4.identity(mat)
-  mat4.translate(mat, [75, 0, 0])
+  mat4.translate(mat, mat, [75, 0, 0])
   const m = mat4.create()
   hpub().toUpperCase().match(/.{1,16}/g).map((str, i) => {
     mat4.copy(m, mat)
