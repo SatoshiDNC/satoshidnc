6fb1767f4c7f65234e7a40dde967286bf847f11b
rleed <rleed@satoshidnc.com>
2024-08-21 21:22:44 -0400

    dev

--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -2,7 +2,7 @@ import { hpub } from './key.js'
 
 export const roomBar = v = new fg.SliceView(null, 'tl', 50)
 v.name = Object.keys({roomBar}).pop()
-v.bgColor = [1,0,0,1]
+v.bgColor = [0.5,0.5,0.5,1]
 v.fontColor = [1,1,1,1]
 v.white = [1,1,1,1]
 v.yellow = [1,0.8,0,1]
