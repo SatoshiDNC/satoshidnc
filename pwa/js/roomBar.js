aa06bafcbf9c30c31bcc4bc54f80dcd70f6f212b
rleed <rleed@satoshidnc.com>
2024-08-21 22:25:52 -0400

    dev

--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -2,8 +2,8 @@ import { hpub } from './key.js'
 
 export const roomBar = v = new fg.View()
 v.name = Object.keys({roomBar}).pop()
-v.designHeight = 32
-v.bgColor = [0.5,0.5,0.5,1]
+v.designHeight = 147
+v.bgColor = [0.043,0.078,0.106,1]
 v.fontColor = [1,1,1,1]
 v.white = [1,1,1,1]
 v.yellow = [1,0.8,0,1]
@@ -15,12 +15,14 @@ v.renderFunc = function() {
   gl.clearColor(...v.bgColor)
   gl.clear(gl.COLOR_BUFFER_BIT)
   const mat = mat4.create()
+  mat4.identity(mat)
+  mat4.translate(mat, [75, 0, 0])
+  const m = mat4.create()
   hpub().toUpperCase().match(/.{1,16}/g).map((str, i) => {
-    mat4.identity(mat)
-    console.log(str, i)
+    mat4.copy(m, mat)
     const x = -0.5 // (v.sw - nybbleFont.calcWidth(str))/2
     const y = 8.5 // (v.sh)/2
-    nybbleFont.draw(x,y + i*8, str, v.shirtColor[i], v.mat, mat)
+    nybbleFont.draw(x,y + i*8, str, v.shirtColor[i], v.mat, m)
   })
 }
 
