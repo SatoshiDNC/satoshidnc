51249e7348c12e6aae99e524224e14d3957fa2e2
rleed <rleed@satoshidnc.com>
2024-08-21 20:49:40 -0400

    dev

--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -1,3 +1,5 @@
+import { hpub } from './key'
+
 const roomBar = v = new fg.SliceView(null, 'tl', 50)
 v.name = Object.keys({roomBar}).pop()
 v.bgColor = [1,0,0,1]
@@ -8,7 +10,7 @@ v.renderFunc = function() {
   gl.clear(gl.COLOR_BUFFER_BIT)
   const mat = mat4.create()
   mat4.identity(mat)
-  const str = "0123455789ABCDEF"
+  const str = hpub()
   const x = (v.sw - nybbleFont.calcWidth(str))/2
   const y = (v.sh)/2
   nybbleFont.draw(x,y, str, v.fontColor, v.mat, mat)
