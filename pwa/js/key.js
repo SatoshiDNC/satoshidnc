d3ade452220f060d6dcc213090e2ce71c822c370
rleed <rleed@satoshidnc.com>
2024-08-26 15:30:44 -0400

    dev

--- pwa/js/key.js
+++ pwa/js/key.js
@@ -3,6 +3,8 @@ import * as nip19 from 'nostr-tools/nip19'
 import { Buffer } from 'buffer'
 
 export function hsec() { return window.localStorage.getItem('hsec')||(window.localStorage.setItem('hsec', Buffer.from(crypto.getRandomValues(new Uint8Array(32))).toString('hex'))||window.localStorage.getItem('hsec')) }
-export function hpub() { return getPublicKey(Buffer.from(hsec(), 'hex')) }
+export function bsec() { return Buffer.from(hsec(), 'hex') }
+export function nsec() { return nip19.nsecEncode(bsec()) }
+export function hpub() { return getPublicKey(bsec()) }
+export function bpub() { return Buffer.from(hpub(), 'hex') }
 export function npub() { return nip19.npubEncode(hpub()) }
-export function nsec() { return nip19.nsecEncode(Buffer.from(hsec(), 'hex')) }
