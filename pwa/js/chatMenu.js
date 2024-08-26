98f7accc2ece25c860a66ab1ca031d7d49ca8829
rleed <rleed@satoshidnc.com>
2024-08-26 10:02:35 -0400

    dev

---
+++ pwa/js/chatMenu.js
@@ -0,0 +1,32 @@
+import { chatRoom } from './chat.js'
+
+export const chatMenuView = v = new fg.View(null)
+v.name = Object.keys({chatMenuView}).pop()
+v.bgColor = [0x12/0xff, 0x1b/0xff, 0x22/0xff, 1]
+v.loadingColor = [1-v.bgColor[0],1-v.bgColor[1],1-v.bgColor[2],1]
+v.loadingText = `Menu`//`${window.devicePixelRatio}, ${vvs = window.visualViewport.scale}`
+v.setText = function(text) {
+  this.loadingText = text
+  console.log('splash:', text)
+}
+v.finish = function(text) {
+  if (text) this.loadingText = text
+}
+v.renderFunc = function() {
+  const v = this
+  gl.clearColor(...v.bgColor)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  const mat = mat4.create()
+  mat4.identity(mat)
+  const str = v.loadingText
+  const x = (v.sw - defaultFont.calcWidth(str))/2
+  const y = (v.sh)/2
+  defaultFont.draw(x,y, str, v.loadingColor, v.mat, mat)
+}
+
+export const chatMenu = v = new fg.OverlayView(null)
+v.name = Object.keys({chatMenu}).pop()
+v.a = chatMenuView; chatMenuView.parent = v
+v.b = chatRoom; chatRoom.parent = v
+
+export const chatMenuRoot = chatMenu
--- pwa/js/roomBar.js
+++ pwa/js/roomBar.js
@@ -1,3 +1,4 @@
+import { chatMenuRoot } from './chatMenu.js'
 import { hpub } from './key.js'
 
 let v, g
@@ -15,8 +16,10 @@ v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
   g.actionFlags = fg.GAF_CLICKABLE
   g.label = ':'
   g.clickFunc = function() {
-    const g = this
+    const g = this, v = this.viewport
     console.log(`${g.label} click`)
+    fg.pushRoot(chatMenuRoot)
+    console.log(`changed root`)
   }
 v.layoutFunc = function() {
   const v = this
@@ -42,13 +45,13 @@ v.renderFunc = function() {
   })
   mat4.identity(mat)
   mat4.translate(mat, mat, [190, 68, 0])
-  mat4.scale(mat, mat, [1/14*35, 1/14*35, 1])
+  mat4.scale(mat, mat, [35/14, 35/14, 1])
   x = 0, y = 0
   defaultFont.draw(x,y, 'Satoshi, D.N.C.', v.textColor, v.mat, mat)
   
   mat4.identity(mat)
   mat4.translate(mat, mat, [190, 116, 0])
-  mat4.scale(mat, mat, [1/14*23, 1/14*23, 1])
+  mat4.scale(mat, mat, [23/14, 23/14, 1])
   x = 0, y = 0
   defaultFont.draw(x,y, 'Online', v.textColor, v.mat, mat)
 
@@ -58,4 +61,3 @@ v.renderFunc = function() {
   mat4.scale(mat, mat, [g.h/11, g.h/11, 1])
   iconFont.draw(0,0, g.label, v.textColor, v.mat, mat)
 }
-
