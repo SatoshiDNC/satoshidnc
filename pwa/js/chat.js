00a77e10f9e480578a6041b0adaadd356e02522c
rleed <rleed@satoshidnc.com>
2024-08-26 19:53:42 -0400

    dev

--- pwa/js/chat.js
+++ pwa/js/chat.js
@@ -80,39 +80,39 @@ v.bgColor = [0,0,0,1]
 v.easingState = 1
 v.easingValue = 0
 v.easingRate = 0.033
-v.renderFunc = function() {
-  const v = this
-  if (v.easingState) {
-    if (v.easingState == 1) {
-      v.easingValue += v.easingRate
-      if (v.easingValue >= 1) {
-        v.easingValue = 1
-        v.easingState = 0
-      }
-    }
-    if (v.easingState == -1) {
-      v.easingValue -= v.easingRate
-      if (v.easingValue < 0) {
-        v.easingValue = 0
-        v.easingState = 0
-        fg.setRoot(roomBar.backGad.target)
-      }
-    }
-    v.setRenderFlag(true)
-  }
-  // if (v.easingValue < 1) {
-  //   const m = mat4.create()
+// v.renderFunc = function() {
+//   const v = this
+//   if (v.easingState) {
+//     if (v.easingState == 1) {
+//       v.easingValue += v.easingRate
+//       if (v.easingValue >= 1) {
+//         v.easingValue = 1
+//         v.easingState = 0
+//       }
+//     }
+//     if (v.easingState == -1) {
+//       v.easingValue -= v.easingRate
+//       if (v.easingValue < 0) {
+//         v.easingValue = 0
+//         v.easingState = 0
+//         fg.setRoot(roomBar.backGad.target)
+//       }
+//     }
+//     v.setRenderFlag(true)
+//   }
+//   // if (v.easingValue < 1) {
+//   //   const m = mat4.create()
 
-  //   mainShapes.useProg2()
-  //   gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array([v.bgColor[0],v.bgColor[1],v.bgColor[2], (1-v.easingValue)*.5]))
-  //   gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
-  //   mat4.identity(m)
-  //   //mat4.translate(m,m, [0, v.menuY, 0])
-  //   mat4.scale(m,m, [v.sw, v.sh, 1])
-  //   gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
-  //   mainShapes.drawArrays2('rect')
-  // }
-}
+//   //   mainShapes.useProg2()
+//   //   gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array([v.bgColor[0],v.bgColor[1],v.bgColor[2], (1-v.easingValue)*.5]))
+//   //   gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
+//   //   mat4.identity(m)
+//   //   //mat4.translate(m,m, [0, v.menuY, 0])
+//   //   mat4.scale(m,m, [v.sw, v.sh, 1])
+//   //   gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
+//   //   mainShapes.drawArrays2('rect')
+//   // }
+// }
 chatRoot.easeOut = function() {
   const v = this
   chatRoot.easingState = -1
