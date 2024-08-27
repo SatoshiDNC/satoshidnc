e97f9d2df4dd6b8ddab8e12594e3d3660ccaff33
rleed <rleed@satoshidnc.com>
2024-08-26 20:00:02 -0400

    dev

--- pwa/js/chat.js
+++ pwa/js/chat.js
@@ -84,11 +84,11 @@ v.renderFunc = function() {
   const v = this
   if (v.easingState) {
     if (v.easingState == 1) {
-      v.easingValue += v.easingRate
-      if (v.easingValue >= 1) {
-        v.easingValue = 1
-        v.easingState = 0
-      }
+      // v.easingValue += v.easingRate
+      // if (v.easingValue >= 1) {
+      //   v.easingValue = 1
+      //   v.easingState = 0
+      // }
     }
     if (v.easingState == -1) {
       v.easingValue -= v.easingRate
