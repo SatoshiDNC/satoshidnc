edda53a7dc5788df41e7f613c293af008baf817e
rleed <rleed@satoshidnc.com>
2024-08-27 21:35:02 -0400

    dev

--- pwa/js/home.js
+++ pwa/js/home.js
@@ -8,60 +8,33 @@ v.designSize = 1080*1825
 v.splashMode = 0
 v.frameTimes = []
 v.bgColor = [0x0b/0xff, 0x14/0xff, 0x1b/0xff, 1]
+v.titleColor = [0xe9/0xff, 0xed/0xff, 0xee/0xff, 1]
+v.subtitleColor = [0x8d/0xff, 0x95/0xff, 0x98/0xff, 1]
 v.loadingColor = [1-v.bgColor[0],1-v.bgColor[1],1-v.bgColor[2],1]
 v.loadingText = `Satoshi, D.N.C.`//`${window.devicePixelRatio}, ${vvs = window.visualViewport.scale}`
-v.setText = function(text) {
-  this.loadingText = text
-  console.log('splash:', text)
-}
-v.finish = function(text) {
-  if (text) this.loadingText = text
-}
 v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.clear(gl.COLOR_BUFFER_BIT)  
   const mat = mat4.create()
-  mat4.identity(mat)
-  if (v.splashMode == 0 || v.splashMode == 1) {
-    const str = v.loadingText
-    const x = (v.sw - defaultFont.calcWidth(str))/2
-    const y = (v.sh)/2
-    defaultFont.draw(x,y, str, v.loadingColor, v.mat, mat)
 
-    const now = new Date().getTime()
-    let nowPrime = now
-    if (v.splashMode == 0) {
-      v.frameTimes.push(now)
-      if (v.frameTimes.length > 30) {
-        nowPrime = v.frameTimes.splice(0,1)
-      }
-    } else if (v.splashMode = 1) {
-      if (v.loadingColor[0] > 0) {
-        let val = Math.max(0,v.loadingColor[0]-1/30)
-        v.loadingColor = [val, val, val, 1]
-      } else {
-        v.splashMode = 2
-      }
-    }
-    if (v.loadingFinished) {
-      if (v.splashMode == 0) {
-        if (now - nowPrime >= 490) {
-          v.splashMode = 1
-        } else {
-          v.splashMode = 2
-        }
-      }
-    }
-  } else if (v.splashMode == 2) {
-    loadAccount()
-    if (pinsettings.enablepin.state && securitypane.mode === 'unlock') {
-      fg.setRoot(securitypane)
-    } else {
-      fg.setRoot(home)
-    }
+  const contacts = [
+    { pubkey: hpub(), name: 'You', xmitDate: Date.now(), xmitText: 'link' },
+    { pubkey: hpub(), name: 'Rando', xmitDate: Date.now(), xmitText: 'tbd' },
+  ]
+
+  let i = 0
+  for (c of contacts) {
+    mat4.identity(mat)
+    const str = c.name
+    const x = 192
+    const y = 253 + 200 * i
+    defaultFont.draw(x,y, str, v.titleColor, v.mat, mat)
+    i++
   }
-  // this.setRenderFlag(true)
+
+  // item spacing: 200
+  // photo x=31, w=127, h=127
 }
 
 export const homeOverlayView = v = new fg.View(null)
