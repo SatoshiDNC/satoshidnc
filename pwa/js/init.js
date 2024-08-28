df3ebcf75c14ebdb01b504dd5d434aaf6753ca13
rleed <rleed@satoshidnc.com>
2024-08-27 20:00:58 -0400

    dev

--- pwa/js/init.js
+++ pwa/js/init.js
@@ -1,7 +1,9 @@
+import { appBar } from './appBar.js'
 import { roomBar } from './roomBar.js'
 import { chatMenuRoot } from './chatMenu.js'
 import { chatRoot } from './chat.js'
 import { homeRoot } from './home.js'
 
+appBar.menuGad.target = chatMenuRoot
 roomBar.menuGad.target = chatMenuRoot
 roomBar.backGad.root = chatRoot, roomBar.backGad.target = homeRoot
