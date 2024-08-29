559f23c0d3cf487234f32306b27faa44be37f7bb
rleed <rleed@satoshidnc.com>
2024-08-28 20:32:52 -0400

    dev

--- pwa/js/view/home-chat/new-chat/new-contact/bar-top.js
+++ pwa/js/view/home-chat/new-chat/new-contact/bar-top.js
@@ -4,27 +4,17 @@ v.name = Object.keys({barTop}).pop()
 v.designHeight = 147
 v.bgColor = [0.043,0.078,0.106,1]
 v.textColor = [1,1,1,1]
-v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
+v.gadgets.push(g = v.backGad = new fg.Gadget(v))
   g.actionFlags = fg.GAF_CLICKABLE
-  g.label = ':'
+  g.label = '\x08'
+  g.x = 43, g.y = 52
+  g.w = 42, g.h = 42
   g.font = iconFont
-  g.fontSize = 11
+  g.fontSize = 13
+  g.autoHull()
   g.clickFunc = function() {
     const g = this, v = this.viewport
-    if (fg.getRoot() !== g.target || g.target.easingState() == -1) {
-      g.target?.easeIn?.()
-    } else {
-      g.target?.easeOut?.()
-    }
-  }
-v.gadgets.push(g = v.searchGad = new fg.Gadget(v))
-  g.actionFlags = fg.GAF_CLICKABLE
-  g.label = '?'
-  g.font = iconFont
-  g.fontSize = 11
-  g.clickFunc = function() {
-    const g = this, v = this.viewport
-    console.log(`click '${g.label}'`)
+    g.root.easeOut(g.target)
   }
 v.gadgets.push(g = v.lawGad = new fg.Gadget(v))
   g.actionFlags = fg.GAF_CLICKABLE
@@ -37,18 +27,6 @@ v.gadgets.push(g = v.lawGad = new fg.Gadget(v))
   }
 v.layoutFunc = function() {
   const v = this
-  g = v.menuGad
-  g.x = v.sw - 64
-  g.y = 51
-  g.w = 12
-  g.h = 45
-  g.autoHull()
-  g = v.searchGad
-  g.x = v.sw - 192
-  g.y = 51
-  g.w = 45
-  g.h = 45
-  g.autoHull()
   g = v.lawGad
   g.x = v.sw - 322
   g.y = 51
@@ -62,10 +40,9 @@ v.renderFunc = function() {
   gl.clear(gl.COLOR_BUFFER_BIT)
   const mat = mat4.create()
   mat4.identity(mat)
-  mat4.translate(mat, mat, [42, 97, 0])
-  mat4.scale(mat, mat, [1/14*44, 1/14*44, 1])
-  let x = 0, y = 0
-  defaultFont.draw(x,y, 'Satoshi, D.N.C.', v.textColor, v.mat, mat)
+  mat4.translate(mat, mat, [141, 98, 0])
+  mat4.scale(mat, mat, [46/14, 46/14, 1])
+  defaultFont.draw(0,0, 'New contact', v.textColor, v.mat, mat)
 
   for (g of v.gadgets) {
     mat4.identity(mat)
