41fcab57fb8118677c3d2889bff0274ac474711e
rleed <rleed@satoshidnc.com>
2024-08-28 22:14:59 -0400

    dev

--- pwa/js/view/home-chat/new-chat/new-contact/bar-bot.js
+++ pwa/js/view/home-chat/new-chat/new-contact/bar-bot.js
@@ -2,8 +2,20 @@ let v, g
 export const barBot = v = new fg.View()
 v.name = Object.keys({barBot}).pop()
 v.bgColor = [0x0b/0xff, 0x14/0xff, 0x1b/0xff, 1]
+v.buttonFaceColor = colors.accentButtonFace
+v.buttonTextColor = colors.accentButtonText
 v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
   gl.clear(gl.COLOR_BUFFER_BIT)
+  const m = mat4.create()
+
+  mainShapes.useProg2()
+  gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array(v.buttonFaceColor))
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
+  mat4.identity(m)
+  mat4.translate(m,m, [79, v.sh - 53 - 104, 0])
+  mat4.scale(m,m, [v.sw - 79 - 79, 104, 1])
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
+  mainShapes.drawArrays2('rect')
 }
