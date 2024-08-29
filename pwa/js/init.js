2bc7c0bd0ecc9ba969f1f1fb93db24287a972588
rleed <rleed@satoshidnc.com>
2024-08-28 20:00:06 -0400

    dev

--- pwa/js/init.js
+++ pwa/js/init.js
@@ -2,7 +2,7 @@ import { barTop as homeChatTop } from './view/home-chat/bar-top.js'
 import { barTop as newChatTop } from './view/home-chat/new-chat/bar-top.js'
 import { barTop as chatRoomBar } from './view/chat-room/bar-top.js'
 import { menuRoot } from './menu.js'
-import { chatRoot as homeChat_newChat_root } from './view/chat-room/content.js'
+import { chatRoot } from './view/chat-room/content.js'
 import { homeRoot as homeChat_root } from './view/home-chat/root.js'
 import { overlayView as homeChat_overlay } from './view/home-chat/content.js'
 import { newChatRoot as homeChat_newChat_root } from './view/home-chat/new-chat/root.js'
@@ -11,7 +11,7 @@ import { newContactRoot as homeChat_newChat_newContact_root } from './view/home-
 
 homeChatTop.menuGad.target = menuRoot
 newChatTop.backGad.root = homeChat_newChat_root, newChatTop.backGad.target = homeChat_root
-chatRoomBar.backGad.root = homeChat_newChat_root, chatRoomBar.backGad.target = homeChat_root
+chatRoomBar.backGad.root = chatRoot, chatRoomBar.backGad.target = homeChat_root
 chatRoomBar.menuGad.target = menuRoot
 homeChat_overlay.addGad.root = homeChat_root, homeChat_overlay.addGad.target = homeChat_newChat_root
 homeChat_newChat_content.addContactGad.root = homeChat_newChat_root, homeChat_newChat_content.addContactGad.target = homeChat_newChat_newContact_root
