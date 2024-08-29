2f6ad8047e48339e51b6613096054fa43b5d8049
rleed <rleed@satoshidnc.com>
2024-08-28 20:21:32 -0400

    dev

--- pwa/js/view/home-chat/new-chat/new-contact/content.js
+++ pwa/js/view/home-chat/new-chat/new-contact/content.js
@@ -8,6 +8,7 @@ v.frameTimes = []
 v.bgColor = [0x0b/0xff, 0x14/0xff, 0x1b/0xff, 1]
 v.titleColor = [0xe9/0xff, 0xed/0xff, 0xee/0xff, 1]
 v.hintColor = [0xb5/0xff, 0xb9/0xff, 0xbc/0xff, 1]
+v.iconColor = [0x8d/0xff, 0x95/0xff, 0x98/0xff, 1]
 v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
@@ -19,12 +20,22 @@ v.renderFunc = function() {
   mat4.translate(m,m, [73, 158, 0])
   const s1 = 43/iconFont.calcWidth('\x00')
   mat4.scale(m,m, [s1, s1, 1])
-  iconFont.draw(0,0, '\x00', v.hintColor, v.mat, m)
+  iconFont.draw(0,0, '\x00', v.iconColor, v.mat, m)
 
   mat4.identity(m)
   mat4.translate(m,m, [186, 147, 0])
   const s2 = 33/14
   mat4.scale(m,m, [s2, s2, 1])
   defaultFont.draw(0,0, 'Name on this device', v.hintColor, v.mat, m)
-    
+  
+  mainShapes.useProg2()
+  gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array(v.iconColor))
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
+  mat4.identity(m)
+  mat4.translate(m,m, [183, 167, 0])
+  mat4.scale(m,m, [v.sw - 183 - 73, 3, 1])
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
+  mainShapes.drawArrays2('rect')
+
+
 }
