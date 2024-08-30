58bdcb1f421f0de543e3036ed1d744a054fcf387
rleed <rleed@satoshidnc.com>
2024-08-30 07:38:38 -0400

    dev

--- pwa/js/contacts.js
+++ pwa/js/contacts.js
@@ -1,9 +1,11 @@
+import * as nip19 from 'nostr-tools/nip19'
+
 export const contacts = []
 
 export function newContact(name, npub) {
   return {
     name: name,
-    pubkey: npub,
+    pubkey: nip19.npubDecode(npub),
     xmitDate: new Date(),
     xmitText: 'You reacted "&" to "Ok, thanks for the help!"',
   }
