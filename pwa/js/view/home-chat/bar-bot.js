d10c92ca20e595fc12ea086a37ecc5356a3d0cd5
rleed <rleed@satoshidnc.com>
2024-08-30 19:44:33 -0400

    dev

--- pwa/js/view/home-chat/bar-bot.js
+++ pwa/js/view/home-chat/bar-bot.js
@@ -6,4 +6,13 @@ v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
   gl.clear(gl.COLOR_BUFFER_BIT)
+
+  mainShapes.useProg2()
+  gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array(colors.inactiveDark))
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
+  mat4.identity(m)
+  mat4.translate(m,m, [0, 0, 0])
+  mat4.scale(m,m, [v.sw, 2, 1])
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
+  mainShapes.drawArrays2('rect')
 }
