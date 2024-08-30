2847b3d032504cf0668f491fe93dd3b12513d09a
rleed <rleed@satoshidnc.com>
2024-08-30 16:59:45 -0400

    dev

--- pwa/js/view/splash.js
+++ pwa/js/view/splash.js
@@ -9,11 +9,9 @@ v.bgColor = [0,0,0,1]
 v.loadingColor = [1-v.bgColor[0],1-v.bgColor[1],1-v.bgColor[2],1]
 v.loadingText = `PWA`//`${window.devicePixelRatio}, ${vvs = window.visualViewport.scale}`
 v.setText = function(text) {
+  if (text == this.loadingText) return
   this.loadingText = text
-  console.log('splash:', text)
-}
-v.finish = function(text) {
-  if (text) this.loadingText = text
+  console.log('init:', text)
 }
 v.renderFunc = function() {
   const v = this
@@ -27,38 +25,38 @@ v.renderFunc = function() {
     const y = (v.sh)/2
     defaultFont.draw(x,y, str, v.loadingColor, v.mat, mat)
 
-    const now = new Date().getTime()
-    let nowPrime = now
-    if (v.splashMode == 0) {
-      v.frameTimes.push(now)
-      if (v.frameTimes.length > 30) {
-        nowPrime = v.frameTimes.splice(0,1)
-      }
-    } else if (v.splashMode = 1) {
-      if (v.loadingColor[0] > 0) {
-        let val = Math.max(0,v.loadingColor[0]-1/30)
-        v.loadingColor = [val, val, val, 1]
-      } else {
-        v.splashMode = 2
-      }
-    }
-    if (v.loadingFinished) {
-      if (v.splashMode == 0) {
-        if (now - nowPrime >= 490) {
-          v.splashMode = 1
-        } else {
-          v.splashMode = 2
-        }
-      }
-    }
-  } else if (v.splashMode == 2) {
-    loadAccount()
-    if (pinsettings.enablepin.state && securitypane.mode === 'unlock') {
-      fg.setRoot(securitypane)
-    } else {
-      fg.setRoot(home)
-    }
-  }
+  //   const now = new Date().getTime()
+  //   let nowPrime = now
+  //   if (v.splashMode == 0) {
+  //     v.frameTimes.push(now)
+  //     if (v.frameTimes.length > 30) {
+  //       nowPrime = v.frameTimes.splice(0,1)
+  //     }
+  //   } else if (v.splashMode = 1) {
+  //     if (v.loadingColor[0] > 0) {
+  //       let val = Math.max(0,v.loadingColor[0]-1/30)
+  //       v.loadingColor = [val, val, val, 1]
+  //     } else {
+  //       v.splashMode = 2
+  //     }
+  //   }
+  //   if (v.loadingFinished) {
+  //     if (v.splashMode == 0) {
+  //       if (now - nowPrime >= 490) {
+  //         v.splashMode = 1
+  //       } else {
+  //         v.splashMode = 2
+  //       }
+  //     }
+  //   }
+  // } else if (v.splashMode == 2) {
+  //   loadAccount()
+  //   if (pinsettings.enablepin.state && securitypane.mode === 'unlock') {
+  //     fg.setRoot(securitypane)
+  //   } else {
+  //     fg.setRoot(home)
+  //   }
+  // }
   // this.setRenderFlag(true)
   this.renderFinish?.()
 }
