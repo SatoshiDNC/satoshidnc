488d99fe19c9989c6dcfa139ed34d48a91eb1e38
rleed <rleed@satoshidnc.com>
2024-08-30 20:11:13 -0400

    dev

--- pwa/js/view/home-chat/content.js
+++ pwa/js/view/home-chat/content.js
@@ -43,13 +43,13 @@ v.renderFunc = function() {
 
   for (const g of v.filterGads) {
     const goal = g.label == v.activeFilter? 1: 0
-    // if (g.animValue != goal) {
+    if (g.animValue != goal) {
     //   g.animValue = g.animValue * 0.1 + goal * 0.9
     //   if (Math.abs(goal - g.animValue) < 0.005) {
     //     g.animValue = goal
     //   }
     //   v.setRenderFlag(true)
-    // }
+    }
     // const f1 = g.animValue
     // const f0 = 1 - f1
     // const light = [
