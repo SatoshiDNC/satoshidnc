1735988e9effdf2878ab1355018eee4fe1b8c053
rleed <rleed@satoshidnc.com>
2024-09-01 07:21:29 -0400

    dev

--- pwa/js/db.js
+++ pwa/js/db.js
@@ -1,6 +1,19 @@
 export function init() {
   return new Promise((resolve, reject) => {
-    resolve()
-    //reject('not implemented')
+    const req = window.indexedDB.open('db', 1)
+    req.onsuccess = e => {
+      db = req.result
+      resolve()
+    }
+    req.onerror = e => {
+      reject(e)
+    }
+    req.onupgradeneeded = e => {
+      console.log(`db: upgrading from ${e.oldVersion} to ${e.newVersion}`)
+      let os
+      if (e.oldVersion < 1) {
+        const keys = os = db.createObjectStore(`keys`, { keyPath: 'hpub' })
+      }
+    }
   })
 }
