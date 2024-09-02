594b27af7e168b027846d0126b557cbca0cbc2a9
rleed <rleed@satoshidnc.com>
2024-09-02 10:25:56 -0400

    dev

--- pwa/js/view/chat-room/content.js
+++ pwa/js/view/chat-room/content.js
@@ -26,8 +26,8 @@ v.renderFunc = function() {
   defaultFont.draw(0,0, text, colors.chatInfoText, v.mat, m)
 
   text = [
-    'Your device might be under surveillence,',
-    'so encryption does not guarantee your privacy.',
+    'Your device might be under surveillence;',
+    'encryption does not guarantee your privacy.',
     'Use an offline encryption and signing device.',
   ]
   w = text.map(line => defaultFont.calcWidth(line) * s)
