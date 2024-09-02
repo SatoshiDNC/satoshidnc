326d20d6eac820cecf6484c73b36cd7d926d344f
rleed <rleed@satoshidnc.com>
2024-09-02 10:32:31 -0400

    dev

--- pwa/js/view/chat-room/content.js
+++ pwa/js/view/chat-room/content.js
@@ -28,7 +28,7 @@ v.renderFunc = function() {
   text = [
     'Encryption does not guarantee your privacy',
     'when your device is subject to surveillence.',
-    'Learn about offline encryption and signing.',
+    'Tap for offline encryption and signing info.',
   ]
   w = text.map(line => defaultFont.calcWidth(line) * s)
   let maxw = w.reduce((p,q)=>Math.max(p,q),0)
