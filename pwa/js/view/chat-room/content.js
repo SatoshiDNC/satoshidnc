ed502cdc122d2e7a7f850c8ec741932c13401562
rleed <rleed@satoshidnc.com>
2024-09-02 09:38:58 -0400

    dev

--- pwa/js/view/chat-room/content.js
+++ pwa/js/view/chat-room/content.js
@@ -3,9 +3,6 @@ import { drawRoundedRect } from '../../draw.js'
 
 export const contentView = v = new fg.View(null)
 v.name = Object.keys({contentView}).pop()
-v.designSize = 640*400
-v.splashMode = 0
-v.frameTimes = []
 v.bgColor = [0x09/0xff, 0x14/0xff, 0x1a/0xff, 1]
 v.setContact = function(contact) {
   console.log(`contact: ${JSON.stringify(contact)}`)
