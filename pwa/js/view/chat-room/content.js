1b2c9332087fbbe8816d68b36f0d4e3a1e68557f
rleed <rleed@satoshidnc.com>
2024-09-02 11:00:23 -0400

    dev

--- pwa/js/view/chat-room/content.js
+++ pwa/js/view/chat-room/content.js
@@ -26,7 +26,8 @@ v.renderFunc = function() {
   defaultFont.draw(0,0, text, colors.chatInfoText, v.mat, m)
 
   text = [
-    'Encryption does not guarantee your privacy',
+    //'Encryption does not guarantee your privacy',
+    'E2E encrypted, without guarantee of privacy',
     'when your device is subject to surveillence.',
     'Tap for offline encryption and signing info.',
   ]
