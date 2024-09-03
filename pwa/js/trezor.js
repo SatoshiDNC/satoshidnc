1b1a35bac84988b7c5f557bb58f73a9102afeb69
rleed <rleed@satoshidnc.com>
2024-09-03 18:49:46 -0400

    dev

--- pwa/js/trezor.js
+++ pwa/js/trezor.js
@@ -172,7 +172,7 @@ export function trezorAction() {
                   console.log('code:', value)
                   break
                 case 2:
-                  console.log('msg:', new TextDecoder().decode(value))
+                  console.log('msg:', new TextDecoder().decode(value.buffer))
                   break
               }
             }
