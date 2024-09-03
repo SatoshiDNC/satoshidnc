e2fb4b2ae2d79b78d8c04d62b25ee22db9fa8624
rleed <rleed@satoshidnc.com>
2024-09-02 21:47:19 -0400

    dev

--- pwa/js/view/chat-room/send-as/menu.js
+++ pwa/js/view/chat-room/send-as/menu.js
@@ -171,7 +171,7 @@ v.renderFunc = function() {
 
   let i = 0
   for (const item of v.items) {
-    drawRect(v, colors.inactive, v.menuX, v.menuY + TITLE_TOP + 41 + 79 + 35 / 2 - 179 / 2, v.menuW, 179)
+    drawRect(v, colors.inactiveDark, v.menuX, v.menuY + TITLE_TOP + 41 + 79 + 35 / 2 - 179 / 2 + v.menuH * f0, v.menuW, 179)
     mat4.identity(mat)
     mat4.translate(mat, mat, [v.menuX + 42, v.menuY + TITLE_TOP + 41 + 44 + 179 * i + v.menuH * f0, 0])
     mat4.scale(mat, mat, [105/32, 105/32, 1])
