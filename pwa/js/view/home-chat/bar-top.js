a25a32118fccfdda5ed2d6345e837af328d66780
rleed <rleed@satoshidnc.com>
2024-09-03 15:10:36 -0400

    dev

--- pwa/js/view/home-chat/bar-top.js
+++ pwa/js/view/home-chat/bar-top.js
@@ -11,6 +11,13 @@ v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
   g.fontSize = 11
   g.handler = function(item) {
     console.log(`id ${JSON.stringify(item)}`)
+    if (item.id == 3) {
+      navigator.usb.requestDevice({ filters: [{ vendorId: 0x53c1 }] }).then(device => {
+        console.log(device)
+      }).catch(e => {
+        console.error(e)
+      })
+    }
   }
   g.items = [
     { id: 1, handler: g.handler, label: 'New group' },
