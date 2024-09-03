010e00115f1447ef4a24fe33711b9115ee1ef9ca
rleed <rleed@satoshidnc.com>
2024-09-03 19:18:35 -0400

    dev

--- pwa/js/trezor.js
+++ pwa/js/trezor.js
@@ -91,14 +91,12 @@ export function trezorAction() {
         console.log(new Uint8Array(res.data.buffer))
         const d = new Uint8Array(res.data.buffer)
         if (new TextDecoder().decode(d.slice(0,3)) == '?##') {
-          console.log('magic found')
           const msgType = d[3]*256 + d[4]
           let remaining = d[5]*16777216 + d[6]*65536 + d[7]*256 + d[8]
           let payload = []
           payload.splice(0, 0, ...d.slice(9,9 + Math.min(55, remaining)))
           remaining -= 55
           const finisher = msg => {
-            console.log('finisher', msg)
             switch (msgType) {
               case OUT_Failure: resolve({ msgType, ...msgFailure(msg) }); break
               default: reject('unexpected response from Trezor')
@@ -170,7 +168,6 @@ export function trezorAction() {
     let code
     let message
     while (msg.length > 0) {
-      console.log(msg)
       let { param, type, value } = readTLV(msg)
       switch (param) {
         case 1:
@@ -198,7 +195,6 @@ export function trezorAction() {
     console.log(`out:`, d)
     return readFunc()
   }).then(res => {
-    console.log(`in:`, res)
     alert(JSON.stringify(res))
   }).catch(e => {
     console.error(e)
