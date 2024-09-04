f79f80b3ebcee3d68032428dcc05557f56c698cc
rleed <rleed@satoshidnc.com>
2024-09-04 11:46:23 -0400

    dev

--- pwa/js/trezor.js
+++ pwa/js/trezor.js
@@ -434,9 +434,6 @@ export function trezorSign(message) {
         ...paramVarInt(1, (  44 | 0x80000000) >>> 0), // 44' hardened purpose code (BIP 43/44)
         ...paramVarInt(1, (0 | 0x80000000) >>> 0), // 1237' hardened wallet type = Nostr (BIP 44/SLIP 44)
         ...paramVarInt(1, (   0 | 0x80000000) >>> 0), // 0' hardened account number (BIP 44)
-        ...paramVarInt(1, (   0 | 0x00000000) >>> 0), // 0' non-hardened slot
-        ...paramVarInt(1, (   0 | 0x00000000) >>> 0), // 0' non-hardened slot
-        ...paramVarInt(1, (   0 | 0x00000000) >>> 0), // 0' non-hardened slot
         ...paramString(2, message), // message to sign
       ]
       return device.transferOut(1, new Uint8Array([...new TextEncoder().encode('?##'), ...twoByte(IN_SignMessage), ...fourByte(buf.length), ...buf])).then(r => {
