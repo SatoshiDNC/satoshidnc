21b67a1fe0b20648629967d30bd83e1812f3c1f2
rleed <rleed@satoshidnc.com>
2024-09-04 10:00:34 -0400

    dev

--- pwa/js/trezor.js
+++ pwa/js/trezor.js
@@ -346,6 +346,7 @@ export function trezorGetNostrPubKey() {
         ...paramVarInt(1, (  44 | 0x80000000) >>> 0), // 44' hardened purpose code (BIP 43/44)
         ...paramVarInt(1, (1237 | 0x80000000) >>> 0), // 1237' hardened wallet type = Nostr (BIP 44/SLIP 44)
         ...paramVarInt(1, (   0 | 0x80000000) >>> 0), // 0' hardened account number (BIP 44)
+        ...paramVarInt(3, 1), // show on display
       ]
       return device.transferOut(1, new Uint8Array([...new TextEncoder().encode('?##'), ...twoByte(IN_GetPublicKey), ...fourByte(buf.length), ...buf])).then(r => {
         return handleResult(r)
