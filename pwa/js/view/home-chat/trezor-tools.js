caae64654590e1962a9f7ccbaaa99c8791c51827
rleed <rleed@satoshidnc.com>
2024-09-05 16:32:04 -0400

    dev

--- pwa/js/view/home-chat/trezor-tools.js
+++ pwa/js/view/home-chat/trezor-tools.js
@@ -139,7 +139,7 @@ v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
         case SIGN_MSG:
           const testEvent = {
             kind: 1,
-            created_at: Date.now(),
+            created_at: Date.now() / 1000,
             content: 'test',
             tags: [
             ],
