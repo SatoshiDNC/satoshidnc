5d1671316e80a6fd00d37b6d6b4b4e5c9dab90f1
rleed <rleed@satoshidnc.com>
2024-09-13 13:41:38 -0400

    dev

--- pwa/js/keys.js
+++ pwa/js/keys.js
@@ -25,3 +25,48 @@ import { db } from './db.js'
 // export function signEvent(event, pubkey) {
 //   return finalizeEvent(event, getSecretKey(pubkey))
 // }
+
+export const keyViewDependencies = []
+
+export const keys = []
+
+export function addSecretKey(hpub, hsec) {
+  const tr = db.transaction('keys', 'readwrite', { durability: 'strict' })
+  const os = tr.objectStore('keys')
+  const req = os.put({ hpub, keyType: 'literal', hsec })
+  req.onsuccess = (e) => {
+    reloadKeys()
+  }
+}
+
+export function addTrezorKey(hpub, derPath) {
+  const tr = db.transaction('keys', 'readwrite', { durability: 'strict' })
+  const os = tr.objectStore('keys')
+  const req = os.put({ hpub, keyType: 'trezor', derivation: derPath })
+  req.onsuccess = (e) => {
+    reloadKeys()
+  }
+}
+
+export function reloadKeys() {
+  const tr = db.transaction('keys', 'readonly')
+  const os = tr.objectStore('keys')
+  const req = os.openCursor()
+  req.onerror = function(e) {
+     console.err(e)
+  }
+  const newList = []
+  req.onsuccess = function(e) {
+    let cursor = e.target.result
+    if (cursor) {
+      let v = cursor.value
+      console.log(JSON.stringify(v))
+      newList.push({ hpub: v.hpub, keyType: v.keyType })
+      cursor.continue()
+    } else {
+      keys.length = 0
+      keys.push(...newList)
+      keyViewDependencies.map(v => v.setRenderFlag(true))
+    }
+  }
+}
