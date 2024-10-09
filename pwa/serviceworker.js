8e8b810b8a25de743a0e30c46f813bab837d67fb
rleed <rleed@satoshidnc.com>
2024-10-09 11:43:35 -0300

    dev

--- pwa/serviceworker.js
+++ pwa/serviceworker.js
@@ -4,8 +4,18 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('Service worker activated')
 })
+const MINUTE = 60 * 1000
+let minutely
 self.addEventListener('sync', event => {
   console.log('sync', event.tag)
+  let now = Date.now()
+  switch (event.tag) {
+    case 'minutely':
+      if (now - minutely < MINUTE) break
+      minutely = now
+      minutelyTasks()
+      break
+  }
 })
 self.addEventListener('periodicsync', event => {
   console.log('periodicsync', event.tag)
@@ -13,3 +23,6 @@ self.addEventListener('periodicsync', event => {
   //   event.waitUntil(sendMessages())
   // }
 })
+function minutelyTasks() {
+  console.log('run minutely')
+}
