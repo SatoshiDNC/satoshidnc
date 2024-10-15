cd486731194f0890aa926383e329a3283b60c95c
rleed <rleed@satoshidnc.com>
2024-10-15 16:31:42 -0300

    dev

--- pwa/js/contacts.js
+++ pwa/js/contacts.js
@@ -44,7 +44,7 @@ export function reloadContacts() {
     } else {
       contacts.length = 0
       contacts.push(...newList)
-      console.log(`${JSON.stringify(contacts)}`)
+      console.log(`contacts: ${JSON.stringify(contacts)}`)
       contactViewDependencies.map(v => v.setRenderFlag(true))
     }
   }
