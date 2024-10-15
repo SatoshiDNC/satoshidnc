0594ef82ef6aa0517c87d94a631a02f55c0a7f56
rleed <rleed@satoshidnc.com>
2024-10-15 08:44:21 -0300

    dev

--- pwa/js/sw/startup.js
+++ pwa/js/sw/startup.js
@@ -1,3 +1,9 @@
+const TAG = 'START'
 export function startupTasks() {
-  console.log('run startup tasks')
+  console.log(`[${TAG}] getting relay list`)
+  nostrWatchRelays().then(onlineRelays => {
+    console.log(JSON.stringify(onlineRelays))
+  }, error => {
+    console.log(`[${TAG}] couldn't get relay list: ${error}`)
+  })
 }
