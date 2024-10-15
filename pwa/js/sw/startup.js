9a1a4bc6f4df6034beb9c158425ecf78b7d109c7
rleed <rleed@satoshidnc.com>
2024-10-15 09:07:43 -0300

    dev

--- pwa/js/sw/startup.js
+++ pwa/js/sw/startup.js
@@ -1,10 +1,20 @@
 import { nostrWatchRelays } from './nostor.js'
+import { init as dbInit } from '../db.js'
+import { relays, detectRelay } from '../relays.js'
 
 const TAG = 'START'
 export function startupTasks() {
-  console.log(`[${TAG}] getting relay list`)
-  nostrWatchRelays().then(onlineRelays => {
-    console.log(JSON.stringify(onlineRelays))
+  dbInit().then(() => {
+    return nostrWatchRelays()
+  }, error => {
+    console.log(`[${TAG}] database error: ${error}`)
+  }).then(onlineRelays => {
+    onlineRelays.map(relay => {
+      if (!relays.includes(relay)) {
+        console.log(`[${TAG}] cognized new relay: ${JSON.stringify(relay)}`)
+        detectRelay(relay)
+      }
+    })
   }, error => {
     console.log(`[${TAG}] couldn't get relay list: ${error}`)
   })
