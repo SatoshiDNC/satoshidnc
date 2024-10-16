414c08056fe6b38e8434aeddedec464433786dbd
rleed <rleed@satoshidnc.com>
2024-10-16 06:55:10 -0300

    dev

--- pwa/js/view/util.js
+++ pwa/js/view/util.js
@@ -4,7 +4,7 @@ export const setEasingParameters = function(v) {
   v.easingRate = 0.033
   v.renderFinish = function() {
     const v = this
-    console.log('renderFinish', v.easingValue, v.easingRate, v.easingState, v.easingTarget?.name)
+    console.log(`${v.name} renderFinish`, v.easingValue, v.easingRate, v.easingState, v.easingTarget?.name)
     if (v.easingState) {
       if (v.easingState == 1) {
         v.easingValue += v.easingRate
@@ -39,8 +39,8 @@ export const setEasingParameters = function(v) {
     }
   }
   v.easeOut = function(targetView) {
-    console.log('official easeOut')
     const v = this
+    console.log(`${v.name} official easeOut`)
     v.easingState = -1
     v.easingRate = 0.1
     v.easingTarget = targetView
