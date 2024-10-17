f95c6b534b8b6f7a49ebf42b8fd48f049fa4e239
rleed <rleed@satoshidnc.com>
2024-10-17 07:52:21 -0300

    dev

--- pwa/js/content.js
+++ pwa/js/content.js
@@ -5,18 +5,20 @@ export function aggregateEvent(e) {
   const now = Date.now()
   return new Promise((resolve, reject) => {
     if (!e || !e.id || !e.sig || !e.pubkey) reject('invalid event')
-    console.log(`[${TAG}] event`, JSON.stringify(e))
     const tr = db.transaction('events', 'readwrite', { durability: 'strict' })
     const os = tr.objectStore('events')
     const req = os.get(e.id)
     req.onsuccess = () => {
-      console.log(`[${TAG}]`, e.id, req, req.result)
-      resolve()
+      if (!req.result) {
+        console.log(`[${TAG}] new event`, JSON.stringify(e))
+        const req = os.put({ id: e.id, firstSeen: now, data: e })
+        req.onsuccess = () => {
+          resolve()
+        }
+      } else {
+        resolve()
+      }
     }
 
-    // const req = os.put({ id: e.id, firstSeen: now, data: e })
-    // req.onsuccess = (e) => {
-    //   resolve()
-    // }
   })
 }
