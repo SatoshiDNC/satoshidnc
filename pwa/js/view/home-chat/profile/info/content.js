c91d203b0d40b4b55114a78397a682c5be7e5d59
rleed <rleed@satoshidnc.com>
2024-10-17 18:34:17 -0300

    dev

--- pwa/js/view/home-chat/profile/info/content.js
+++ pwa/js/view/home-chat/profile/info/content.js
@@ -111,7 +111,7 @@ v.setContact = function(hpub) {
         const mat = mat4.create()
         let t,tw,ts
 
-        t = `${g.data.kind} · ${kindInfo.filter(r=>r.kindMax?r.kind<=g.data.kind&&g.data.kind<=r.kindMax:r.kind==g.data.kind)?.[0]?.desc.toUpperCase()}`
+        t = `${g.data.kind} · ${(''+kindInfo.filter(r=>r.kindMax?r.kind<=g.data.kind&&g.data.kind<=r.kindMax:r.kind==g.data.kind)?.[0]?.desc).toUpperCase()}`
         tw = defaultFont.calcWidth(t)
         ts = 20/14
         mat4.identity(mat)
