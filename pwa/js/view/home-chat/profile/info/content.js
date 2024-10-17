45f84909751a4ede05f90c1561266171c6beaae2
rleed <rleed@satoshidnc.com>
2024-10-17 12:11:09 -0300

    dev

--- pwa/js/view/home-chat/profile/info/content.js
+++ pwa/js/view/home-chat/profile/info/content.js
@@ -1,7 +1,7 @@
 import { setEasingParameters } from '../../../util.js'
 import { drawAvatar, alpha } from '../../../../draw.js'
 import { getPersonalData as get } from '../../../../personal.js'
-import { pingFeed } from '../../../../content.js'
+import { pingFeed, getFeed } from '../../../../content.js'
 
 const TAG = 'INFO'
 
