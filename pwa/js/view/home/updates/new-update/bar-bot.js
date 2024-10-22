c40cc77d8b659bb00a76fde398e3f24812ae989c
rleed <rleed@satoshidnc.com>
2024-10-22 18:52:56 -0300

    dev

--- pwa/js/view/home/updates/new-update/bar-bot.js
+++ pwa/js/view/home/updates/new-update/bar-bot.js
@@ -21,10 +21,11 @@ for (const pane of v.panes) {
     v.activeLabel = g.label
     const pane = v.panes.filter(p => p.label == v.activeLabel)[0]
     console.log(pane)
-    pane.view.c = barBot; barBot.parent = pane.view
-    pane.view.easingValue = 1
-    pane.view.setRenderFlag(true)
-    fg.setRoot(pane.view)
+    v.relayout()
+    // pane.view.c = barBot; barBot.parent = pane.view
+    // pane.view.easingValue = 1
+    // pane.view.setRenderFlag(true)
+    // fg.setRoot(pane.view)
   }
 }
 v.activeLabel = v.paneGads[0].label
