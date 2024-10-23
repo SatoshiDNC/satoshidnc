9c47cb56a3dffa181590795f96cf8d561690dc97
rleed <rleed@satoshidnc.com>
2024-10-23 14:00:41 -0300

    dev

--- pwa/js/view/home/updates/display/root.js
+++ pwa/js/view/home/updates/display/root.js
@@ -9,7 +9,16 @@ v.ghostOpacity = 0
 v.bgColor = [0,0,0,1]
 v.a = overlayView; overlayView.parent = v
 v.b = contentView; contentView.parent = v
-v.setContext = function(hpub, hnote) {
-  console.log(hpub, hnote)
+v.setContext = function(updates) {
+  console.log(updates)
+  // let startAt
+  // for (const update of updates) {
+  //   if (!update.viewed) {
+  //     startAt = update
+  //   }
+  // }
+  // if (startAt) {
+  // }
+
 }
 setEasingParameters(v)
