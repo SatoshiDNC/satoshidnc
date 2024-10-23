1819f9ddf544ab64ffe7e4ae681f5d9d26c2a1fc
rleed <rleed@satoshidnc.com>
2024-10-23 15:46:44 -0300

    dev

--- pwa/js/view/home/updates/display/overlay.js
+++ pwa/js/view/home/updates/display/overlay.js
@@ -95,7 +95,7 @@ v.renderFunc = function() {
   mat4.identity(m)
   mat4.translate(m, m, [263, 131, 0])
   mat4.scale(m, m, [24/14, 24/14, 1])
-  defaultFont.draw(0,0, 'Time', v.textColor, v.mat, m)
+  defaultFont.draw(0,0, ''+v.updates[v.currentUpdate].data.created_at, v.textColor, v.mat, m)
 
   // const g = v.addGad
   // mat4.identity(m)
