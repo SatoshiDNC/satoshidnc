f1580c2d6255623d36d1f42d7cb6d0f35dc6f4dd
rleed <rleed@satoshidnc.com>
2024-10-23 07:57:02 -0300

    dev

--- pwa/js/view/util.js
+++ pwa/js/view/util.js
@@ -74,7 +74,7 @@ export function updatePostedAsOf(timeStamp) {
       return `Yesterday`
     }
   }
-  return `age: ${now - timeStamp}`
+  return addedOn(timeStamp)
 }
 
 export function addedOn(timeStamp) {
