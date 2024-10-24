c59499cbb308da7e029377ef6ff0e113373ddbc5
rleed <rleed@satoshidnc.com>
2024-10-24 11:27:23 -0300

    dev

--- pwa/js/view/home/updates/display/overlay.js
+++ pwa/js/view/home/updates/display/overlay.js
@@ -7,14 +7,13 @@ let v, g
 export const overlayView = v = new fg.View(null)
 v.name = Object.keys({overlayView}).pop()
 v.designSize = 1080*1825
-v.bgColor = [0x12/0xff, 0x1b/0xff, 0x22/0xff, 1]
-v.textColor = [1,1,1,1]
-v.buttonFaceColor = colors.accentButtonFace
-v.buttonTextColor = colors.accentButtonText
+v.titleColor = colors.white
+v.subtitleColor = colors.softWhite
 v.gadgets.push(g = v.backGad = new fg.Gadget(v))
   g.actionFlags = fg.GAF_CLICKABLE
   g.x = 39, g.y = 63, g.w = 48, g.h = 48
   g.label = '\x08'
+  g.textColor = colors.white
   g.font = iconFont
   g.fontSize = 13
   g.autoHull()
@@ -43,12 +42,6 @@ v.setContext = function(updates) {
 v.layoutFunc = function() {
   const v = this
   let g
-  // g = v.addGad
-  // g.x = v.sw-189
-  // g.y = v.sh-189
-  // g.w = 147
-  // g.h = 147
-  // g.autoHull()
 }
 v.renderFunc = function() {
   const v = this
@@ -81,23 +74,23 @@ v.renderFunc = function() {
     mat4.identity(m)
     mat4.translate(m, m, [g.x, g.y+g.h, 0])
     mat4.scale(m, m, [g.h/g.fontSize, g.h/g.fontSize, 1])
-    g.font.draw(0,0, g.label, v.textColor, v.mat, m)
+    g.font.draw(0,0, g.label, g.textColor, v.mat, m)
   }
 
-  drawEllipse(v, [1,1,1,1], 126,34, 105,105)
+  drawEllipse(v, v.titleColor, 126,34, 105,105)
   drawEllipse(v, colors.inactive, 129,37, 99,99)
 
   mat4.identity(m)
   mat4.translate(m, m, [263, 82, 0])
   mat4.scale(m, m, [33/14, 33/14, 1])
-  defaultFont.draw(0,0, getAttr(v.updates[v.currentUpdate].hpub, 'name'), v.textColor, v.mat, m)
+  defaultFont.draw(0,0, getAttr(v.updates[v.currentUpdate].hpub, 'name'), v.titleColor, v.mat, m)
 
   const data = v.updates[v.currentUpdate].data
 
   mat4.identity(m)
   mat4.translate(m, m, [263, 131, 0])
   mat4.scale(m, m, [24/14, 24/14, 1])
-  defaultFont.draw(0,0, updatePostedAsOf(data.created_at * 1000, true), v.textColor, v.mat, m)
+  defaultFont.draw(0,0, updatePostedAsOf(data.created_at * 1000, true), v.subtitleColor, v.mat, m)
 
   let t,tw,th,ts
 
@@ -108,16 +101,6 @@ v.renderFunc = function() {
   // mat4.translate(m, m, [15, 200, 0])
   mat4.scale(m, m, [0.85, 0.85, 1])
   defaultFont.draw(20,0, t, alpha(colors.inactive, 0.5), v.mat, m)
-
-  // const g = v.addGad
-  // mat4.identity(m)
-  // mat4.translate(m,m, [g.x, g.y + g.h, 0])
-  // mat4.scale(m,m, [g.w/6, g.h/6, 1])
-  // iconFont.draw(0,0, `\x0a`, v.buttonFaceColor, v.mat, m)
-  // mat4.identity(m)
-  // mat4.translate(m,m, [g.x + 49, g.y + 95, 0])
-  // mat4.scale(m,m, [51/11, 51/11, 1])
-  // iconFont.draw(-2,0, g.label, v.buttonTextColor, v.mat, m)
 
   if (pageTurn) {
     v.currentUpdate += 1
