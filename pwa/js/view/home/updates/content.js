73aafff2eea7055f504376b3ec215a7ebb1c2022
rleed <rleed@satoshidnc.com>
2024-10-24 07:00:16 -0300

    dev

--- pwa/js/view/home/updates/content.js
+++ pwa/js/view/home/updates/content.js
@@ -122,7 +122,7 @@ v.renderFunc = function() {
     console.log(hpub)
     const numUpdates = v.query.results.filter(u => u.hpub == hpub).length
     const newest = v.query.results.filter(u => u.hpub == hpub).reduce((a,c) => Math.max(a,c.data.created_at * 1000), 0)
-    const numViewed = v.query.results.filter(u => u.hpub == hpub).reduce((a,c) => Math.max(a,c.viewed?1:0), 0)
+    const numViewed = v.query.results.filter(u => u.hpub == hpub).reduce((a,c) => a+(c.viewed?1:0), 0)
     const y = i * 200 + ((v.viewed.includes(hpub) && v.recents.length > 0)? 96: 0)
     drawEllipse(v, colors.accent, 32, 492 + y, 147, 147)
     if (numViewed) {
