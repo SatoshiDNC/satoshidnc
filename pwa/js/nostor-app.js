21252a91d3e364586682442f125b2bfb9fab6c50
rleed <rleed@satoshidnc.com>
2024-10-28 15:17:37 -0300

    dev

--- pwa/js/nostor-app.js
+++ pwa/js/nostor-app.js
@@ -5,61 +5,68 @@ import { getRelayStat, setRelayStat } from './stats.js'
 let relays = []
 
 export function getRelay(name) {
-  const relay = relayUrl(name)
   return new Promise((resolve, reject) => {
-    let avgConnect = getRelayStat(relay, 'avgConnect')
-    let socket
-    try {
-      socket = new WebSocket(relay)
-      console.log(`[${TAG}] created socket`, socket.readyState, WebSocket.OPEN)
-    } catch (e) {
-      setRelayStat(relay, 'lastConnect', { time: 0, date: requestTime })
-      console.log(`[${TAG}] error:`, e)
-    }
-    socket.addEventListener('open', event => {
-      const r = {
-        url: relay,
-        state: 'open',
-        socket,
-        send: function(obj) {
-          const r = this
-          console.log(`send ${JSON.stringify(obj)}`)
-          r.socket.send(JSON.stringify(obj))
+    const requestTime = Date.now()
+    const relay = relayUrl(name)
+    let r = relays.filter(r => r.name == name)?.[0]
+    if (r) {
+      resolve(r)
+    } else {
+      let avgConnect = getRelayStat(relay, 'avgConnect')
+      let socket
+      try {
+        socket = new WebSocket(relay)
+        console.log(`[${TAG}] created socket`, socket.readyState, WebSocket.OPEN)
+      } catch (e) {
+        setRelayStat(relay, 'lastConnect', { time: 0, date: requestTime })
+        console.log(`[${TAG}] error:`, e)
+      }
+      socket.addEventListener('close', e => {
+        console.log(`[${TAG}] close`)
+      })
+      socket.addEventListener('error', e => {
+        console.log(`[${TAG}] error`)
+      })
+      socket.addEventListener('message', e => {
+        let m = JSON.parse(e.data)
+        if (m[0] == 'EVENT' && m[1] == 'feed') {
+          const event = m[2]
+          aggregateEvent(hpub, event)
+          setHasData(relay, hpub)
+        } else {
+          console.log(`[${TAG}] message`, JSON.stringify(m))
         }
-      }
-      relays.push(r)
-      const deltaTime = Date.now() - requestTime
-      if (avgConnect) {
-        const w0 = avgConnect.weight, w1 = w0 + 1
-        const t0 = avgConnect.time, t1 = (t0 * w0 + deltaTime) / w1
-        avgConnect = { time: t1, weight: w1 }
-      } else {
-        avgConnect = { time: deltaTime, weight: 1 }
-      }
-      setRelayStat(relay, 'avgConnect', avgConnect)
-      setRelayStat(relay, 'lastConnect', { time: deltaTime, date: requestTime })
-      // if (thisRequestTime == requestTime) {
-      //   debugView.deltaTime = deltaTime
-      //   debugView.setRenderFlag(true)
-      // }
-      console.log(`[${TAG}] open`, deltaTime)
-    })
-    socket.addEventListener('close', e => {
-      console.log(`[${TAG}] close`)
-    })
-    socket.addEventListener('error', e => {
-      console.log(`[${TAG}] error`)
-    })
-    socket.addEventListener('message', e => {
-      let m = JSON.parse(e.data)
-      if (m[0] == 'EVENT' && m[1] == 'feed') {
-        const event = m[2]
-        aggregateEvent(hpub, event)
-        setHasData(relay, hpub)
-      } else {
-        console.log(`[${TAG}] message`, JSON.stringify(m))
-      }
-    })
+      })
+      socket.addEventListener('open', event => {
+        const r = {
+          url: relay,
+          state: 'open',
+          socket,
+          send: function(obj) {
+            const r = this
+            console.log(`send ${JSON.stringify(obj)}`)
+            r.socket.send(JSON.stringify(obj))
+          }
+        }
+        relays.push(r)
+        const deltaTime = Date.now() - requestTime
+        if (avgConnect) {
+          const w0 = avgConnect.weight, w1 = w0 + 1
+          const t0 = avgConnect.time, t1 = (t0 * w0 + deltaTime) / w1
+          avgConnect = { time: t1, weight: w1 }
+        } else {
+          avgConnect = { time: deltaTime, weight: 1 }
+        }
+        setRelayStat(relay, 'avgConnect', avgConnect)
+        setRelayStat(relay, 'lastConnect', { time: deltaTime, date: requestTime })
+        // if (thisRequestTime == requestTime) {
+        //   debugView.deltaTime = deltaTime
+        //   debugView.setRenderFlag(true)
+        // }
+        console.log(`[${TAG}] open`, deltaTime)
+        resolve(r)
+      })
+    }
   })
 }
 
