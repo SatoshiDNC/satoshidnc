10fc33023892b7f7a1236ce4661ac33768c2a8f5
rleed <rleed@satoshidnc.com>
2024-10-30 17:19:10 -0300

    dev

--- pwa/js/periodic.js
+++ pwa/js/periodic.js
@@ -1,6 +1,12 @@
+import { getRelay } from './nostor-app.js'
 import { reqFeed } from './content.js'
 
 export function minutelyUI() {
   console.log('run minutely UI tasks')
-  reqFeed()
+  getRelay('relay.satoshidnc.com').then(relay => {
+    if (!relay.feedRequested) {
+      reqFeed()
+      relay.feedRequested = true
+    }
+  })
 }
--- pwa/js/sw/periodic.js
+++ pwa/js/sw/periodic.js
@@ -1,6 +1,3 @@
-//import { reqFeed } from '../content.js'
-
 export function minutelyTasks() {
   console.log('run minutely tasks')
-  //reqFeed()
 }
