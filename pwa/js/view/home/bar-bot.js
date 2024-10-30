700fcdc87e868e0d37a5af3d12758aee68c5fb4a
rleed <rleed@satoshidnc.com>
2024-10-30 19:59:16 -0300

    dev

--- pwa/js/view/home/bar-bot.js
+++ pwa/js/view/home/bar-bot.js
@@ -84,7 +84,7 @@ v.renderFunc = function() {
     mat4.translate(m,m, [g.x + (g.w - iconFont.calcWidth(g.icon) * s) / 2, g.y + (50 + 84)/2, 0])
     mat4.scale(m,m, [s, s, 1])
     iconFont.draw(0,0, g.icon, v.textColor, v.mat, m)
-    if (g.new) {
+    if (g.new !== undefined) {
       drawEllipse(v, colors.accent, g.x + g.w/2 + 23, g.y + 2, 32, 32)
       if (!isNaN(g.new)) {
         s = 21/14
