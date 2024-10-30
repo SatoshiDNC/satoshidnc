7d1718862d7bd0ca3af593804743884c83acf95a
rleed <rleed@satoshidnc.com>
2024-10-30 19:58:32 -0300

    dev

--- pwa/js/view/home/bar-bot.js
+++ pwa/js/view/home/bar-bot.js
@@ -42,7 +42,7 @@ v.layoutFunc = function() {
   for (const g of v.paneGads) {
     g.x = x + (v.sw / 4 - g.w) / 2, g.y = 33
     g.autoHull()
-    g.new = undefined
+    g.new = NaN
     x += v.sw /4
   }
 }
