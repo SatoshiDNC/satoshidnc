789b5a4b674385048b29a9ac80fba394dbde0881
rleed <rleed@satoshidnc.com>
2024-10-30 05:57:24 -0300

    dev

--- pwa/js/view/home/updates/verbal/overlay.js
+++ pwa/js/view/home/updates/verbal/overlay.js
@@ -69,6 +69,7 @@ v.gadgets.push(g = v.micSendGad = new fg.Gadget(v))
     const g = this, v = g.viewport
     const m = mat4.create()
     drawPill(v, g.buttonFaceColor || v.buttonFaceColor, g.x - g.h, g.y, g.w + g.h, g.h)
+    drawRect(v, colors.inactiveDark, g.x - g.h, g.y, g.h, g.h)
     drawAvatar(v, defaultKey, g.x - g.h, g.y, g.h, g.h)
     const font = g.font || iconFont
     const c = g.icon.codePointAt(0)
