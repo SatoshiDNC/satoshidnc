6053334f3b887f6241fc6327b9eaae96b236d6dd
rleed <rleed@satoshidnc.com>
2024-10-30 09:41:27 -0300

    dev

--- pwa/js/view/home/updates/verbal/overlay.js
+++ pwa/js/view/home/updates/verbal/overlay.js
@@ -308,7 +308,7 @@ v.renderFunc = function() {
     for (let i = 0; i<itemCount; i++) {
       drawAvatar(v, items[i].hpub, x + h*0.05, y - f1*itemHeight*(i+1) + h*0.05, h*0.9, f1*h*0.9)
       mat4.identity(m)
-      mat4.translate(m, m, [x+h, y-f1*itemHeight*(i+1), 0])
+      mat4.translate(m, m, [x+h, y-f1*itemHeight*(i+1)+h/2, 0])
       mat4.scale(m, m, [s, s, 1])
       defaultFont.draw(0,7, 'Test', alpha(colors.white, f1), v.mat, m)
     }
