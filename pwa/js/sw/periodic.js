5c327bb402a6ce76abe0e137b90bb121b3f8e420
rleed <rleed@satoshidnc.com>
2024-10-30 15:21:12 -0300

    dev

--- pwa/js/sw/periodic.js
+++ pwa/js/sw/periodic.js
@@ -1,6 +1,6 @@
-import { reqFeed } from '../content.js'
+//import { reqFeed } from '../content.js'
 
 export function minutelyTasks() {
-  console.log('run minutely tasks')
-  reqFeed()
+  console.log('run minutely tasks.')
+  //reqFeed()
 }
