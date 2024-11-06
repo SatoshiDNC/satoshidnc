60f5a239823cc650c71f2b95cf559d77413159e5
rleed <rleed@satoshidnc.com>
2024-11-06 10:22:48 -0300

    dev

--- pwa/js/view/splash.js
+++ pwa/js/view/splash.js
@@ -17,13 +17,53 @@ v.renderFunc = function() {
   const v = this
   gl.clearColor(...v.bgColor)
   gl.clear(gl.COLOR_BUFFER_BIT)
-  const mat = mat4.create()
-  mat4.identity(mat)
-  const str = v.loadingText
-  const x = (v.sw - defaultFont.calcWidth(str))/2
-  const y = (v.sh)/2
-  defaultFont.draw(x,y, str, v.loadingColor, v.mat, mat)
+  const m = mat4.create()
+  // mat4.identity(mat)
+  // const str = v.loadingText
+  // const x = (v.sw - defaultFont.calcWidth(str))/2
+  // const y = (v.sh)/2
+  // defaultFont.draw(x,y, str, v.loadingColor, v.mat, mat)
 
+  const whitespace = true
+  const maxWidth = v.sw - 42
+  let t,tw,th,ts
+  ts = 50/14
+  const paragraphs = data.content.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
+  const lines = []
+  for (const para of paragraphs) {
+    const words = para.split(' ')
+    while (words.length > 0) {
+      lines.push(words.shift())
+      while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= maxWidth) {
+        let buf = ''
+        while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= maxWidth) {
+          let l = lines.pop()
+          buf = l.substring(l.length-1) + buf
+          l = l.substring(0, l.length-1)
+          lines.push(l)
+        }
+        lines.push(buf)
+      }
+      while (words.length > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) * ts <= maxWidth) {
+        lines.push(lines.pop() + ' ' + words.shift())
+      }
+    }
+  }
+  // tw = lines.reduce((a,c) => Math.max(a, defaultFont.calcWidth(c) * ts, 0))
+  th = lines.length * defaultFont.glyphHeights[65] * ts * 2
+  let i = 1
+  for (let line of lines) {
+    i++
+    if (!line) continue
+    if (whitespace) {
+      line = line.replaceAll(' ', '·')
+    }
+    tw = defaultFont.calcWidth(line) * ts
+    mat4.identity(m)
+    mat4.translate(m, m, [(v.sw - tw)/2, (v.sh - th)/2 + i*defaultFont.glyphHeights[65]*ts*2, 0])
+    mat4.scale(m, m, [ts, ts, 1])
+    defaultFont.draw(0,0, line, v.textColor, v.mat, m)
+  }
   this.renderFinish?.()
 }
 setEasingParameters(v)
