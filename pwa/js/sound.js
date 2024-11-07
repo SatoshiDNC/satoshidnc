0cb80de6cbfedf5fa523e04baaf80da05d22c7ba
rleed <rleed@satoshidnc.com>
2024-11-07 12:15:54 -0300

    dev

--- pwa/js/sound.js
+++ pwa/js/sound.js
@@ -3,22 +3,33 @@ let context
 export function sfx(name, fg) {
   if (!context) {
     context = new AudioContext()
-    var osc1 = context.createOscillator(), osc2 = context.createOscillator()
-    osc1.type = 'triangle', osc1.frequency.value = 493.883 + 1
-    osc2.type = 'triangle', osc2.frequency.value = 493.883 - 2
+
+    // var osc1 = context.createOscillator(), osc2 = context.createOscillator()
+    // osc1.type = 'triangle', osc1.frequency.value = 493.883 + 1
+    // osc2.type = 'triangle', osc2.frequency.value = 493.883 - 2
+
     var volume = context.createGain()
     volume.gain.value = fg? 0.1 : 1.0
-    osc1.connect(volume)
-    osc2.connect(volume)
+
+    // osc1.connect(volume)
+    // osc2.connect(volume)
+
+    const source = context.createBufferSource();
+    fetch(url).then(res => res.arrayBuffer()).then(arrayBuffer => context.decodeAudioData(arrayBuffer)).then(buffer => {
+      source.buffer = buffer
+      source.connect(volume)
+    })
+
     volume.connect(context.destination)
   }
-  var duration = 1
-  var startTime = context.currentTime
-  volume.gain.setValueAtTime(0.1, startTime + 0.5)
-  volume.gain.linearRampToValueAtTime(0, startTime + duration)
-  osc1.start(startTime)
-  osc2.start(startTime)
-  osc1.stop(startTime + duration)
-  osc1.stop(startTime + duration)
+  source.start()
+  // var duration = 1
+  // var startTime = context.currentTime
+  // volume.gain.setValueAtTime(0.1, startTime + 0.5)
+  // volume.gain.linearRampToValueAtTime(0, startTime + duration)
+  // osc1.start(startTime)
+  // osc2.start(startTime)
+  // osc1.stop(startTime + duration)
+  // osc1.stop(startTime + duration)
 }
 
