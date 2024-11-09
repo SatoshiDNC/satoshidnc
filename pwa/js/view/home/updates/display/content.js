d61090e3e29855422ba62f2c74a19df1e2ade06c
rleed <rleed@satoshidnc.com>
2024-11-08 22:25:44 -0300

    dev

--- pwa/js/view/home/updates/display/content.js
+++ pwa/js/view/home/updates/display/content.js
@@ -64,7 +64,7 @@ v.renderKind1 = function(data) {
   let t,tw,th,ts
   ts = 50/14
   //const paragraphs = data.content.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
-  const paragraphs = '\x01\xf6\x0a'
+  const paragraphs = 'A😊B'
   const lines = []
   for (const para of paragraphs) {
     const words = para.split(' ')
