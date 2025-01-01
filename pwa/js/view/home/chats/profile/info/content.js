066ea9beff4bec4648d52a48cdd917ab6cb6b61b
rleed <rleed@satoshidnc.com>
2024-12-31 21:16:48 -0300

    dev

--- pwa/js/view/home/chats/profile/info/content.js
+++ pwa/js/view/home/chats/profile/info/content.js
@@ -42,8 +42,9 @@ v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
     console.log(`id ${JSON.stringify(item)}`)
   }
   g.items = [
-    { id: 1, label: 'Share', handler: () => { navigator.clipboard.writeText(v.hpub).catch(e => console.error(e)) } },
-    { id: 2, handler: g.handler, label: 'Edit' },
+    // { id: 1, label: 'Share', handler: () => { navigator.clipboard.writeText(v.hpub).catch(e => console.error(e)) } },
+    { id: 2, label: 'Copy public key', handler: () => { navigator.clipboard.writeText(v.hpub).catch(e => console.error(e)) } },
+    { id: 3, handler: g.handler, label: 'Edit' },
   ]
   g.clickFunc = function() {
     const g = this, v = this.viewport
