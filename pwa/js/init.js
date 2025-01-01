557a90c43862672b2807f0a4b896bfbb630132a4
rleed <rleed@satoshidnc.com>
2024-12-31 23:40:42 -0300

    dev

--- pwa/js/init.js
+++ pwa/js/init.js
@@ -1,5 +1,5 @@
 import { barTop as home_chats_top } from './view/home/chats/bar-top.js'
-import { barTop as home_chats_new_top } from './view/home/chats/new-chat/bar-top.js'
+import { barTop as home_chats_newChat_top } from './view/home/chats/new-chat/bar-top.js'
 import { barTop as chatRoomBar } from './view/chat-room/bar-top.js'
 import { menuRoot } from './view/menu.js'
 import { chatRoomRoot as chatRoom_root } from './view/chat-room/root.js'
@@ -8,7 +8,7 @@ import { homeRoot as home_updates_root } from './view/home/updates/root.js'
 import { homeRoot as home_communities_root } from './view/home/communities/root.js'
 import { homeRoot as home_calls_root } from './view/home/calls/root.js'
 import { contentView as home_chats_content, overlayView as home_chats_overlay } from './view/home/chats/content.js'
-import { newChatRoot as home_chats_new_root } from './view/home/chats/new-chat/root.js'
+import { newChatRoot as home_chats_newChat_root } from './view/home/chats/new-chat/root.js'
 import { contentView as home_chats_newChat_content} from './view/home/chats/new-chat/content.js'
 import { barTop as home_chats_newChat_newContact_top } from './view/home/chats/new-chat/new-contact/bar-top.js'
 import { newContactRoot as home_chats_newChat_newContact_root } from './view/home/chats/new-chat/new-contact/root.js'
@@ -42,19 +42,19 @@ home_chats_top.menuGad.target = menuRoot
 home_chats_top.menuGad.target2 = home_chats_trezorTools_root
 home_chats_top.menuGad.target3 = home_chats_nostorTools_root
 home_chats_top.menuGad.targetSettings = settings_root
-home_chats_new_top.backGad.root = home_chats_new_root, home_chats_new_top.backGad.target = home_chats_root
-home_chats_new_top.menuGad.root = home_chats_new_root
-home_chats_new_top.menuGad.target = menuRoot
-home_chats_new_top.menuGad.target2 = home_chats_trezorTools_root
+home_chats_newChat_top.backGad.root = home_chats_newChat_root, home_chats_newChat_top.backGad.target = home_chats_root
+home_chats_newChat_top.menuGad.root = home_chats_newChat_root
+home_chats_newChat_top.menuGad.target = menuRoot
+home_chats_newChat_top.menuGad.target2 = home_chats_trezorTools_root
 chatRoomBar.backGad.root = chatRoom_root, chatRoomBar.backGad.target = home_chats_root
 chatRoomBar.menuGad.target = menuRoot
 home_chats_content.listGad.root = home_chats_root, home_chats_content.listGad.target = chatRoom_root, home_chats_content.listGad.target2 = home_updates_display_root
-home_chats_overlay.addGad.root = home_chats_root, home_chats_overlay.addGad.target = home_chats_new_root
-home_chats_newChat_content.newContactGad.root = home_chats_new_root, home_chats_newChat_content.newContactGad.target = home_chats_newChat_newContact_root
-home_chats_newChat_content.listGad.root = home_chats_new_root, home_chats_newChat_content.listGad.target = chatRoom_root
-home_chats_newChat_newContact_top.backGad.root = home_chats_newChat_newContact_top, home_chats_newChat_newContact_top.backGad.target = home_chats_new_root
+home_chats_overlay.addGad.root = home_chats_root, home_chats_overlay.addGad.target = home_chats_newChat_root
+home_chats_newChat_content.newContactGad.root = home_chats_newChat_root, home_chats_newChat_content.newContactGad.target = home_chats_newChat_newContact_root
+home_chats_newChat_content.listGad.root = home_chats_newChat_root, home_chats_newChat_content.listGad.target = chatRoom_root
+home_chats_newChat_newContact_top.backGad.root = home_chats_newChat_newContact_root, home_chats_newChat_newContact_top.backGad.target = home_chats_newChat_root
 home_chats_newChat_newContact_bot.saveGad.formView = home_chats_newChat_newContact_content
-home_chats_newChat_newContact_bot.saveGad.root = home_chats_newChat_newContact_root, home_chats_newChat_newContact_bot.saveGad.target = home_chats_new_root
+home_chats_newChat_newContact_bot.saveGad.root = home_chats_newChat_newContact_root, home_chats_newChat_newContact_bot.saveGad.target = home_chats_newChat_root
 home_chats_profile_info_editContact_top.backGad.root = home_chats_profile_info_editContact_root, home_chats_profile_info_editContact_top.backGad.target = home_chats_profile_info_root
 home_chats_profile_info_editContact_bot.saveGad.root = home_chats_profile_info_editContact_root, home_chats_profile_info_editContact_bot.saveGad.target = home_chats_profile_info_root
 home_chats_profile_info_editContact_bot.saveGad.formView = home_chats_profile_info_editContact_content
