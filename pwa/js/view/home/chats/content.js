e0638efc58b164d9eba37ca7a004e616eaec57aa
rleed <rleed@satoshidnc.com>
2024-12-31 22:41:49 -0300

    dev

--- pwa/js/view/home/chats/content.js
+++ pwa/js/view/home/chats/content.js
@@ -121,7 +121,14 @@ v.renderFunc = function() {
   }
 
   let i = 0
-  for (const c of [ ...contacts.map(c => { return {
+  for (const c of [ ...keys.map(k => { return {
+    hpub: k.hpub,
+    name: (getAttr(k.hpub, 'name') || 'Unnamed') + ' (You)',
+    xmitText: getAttr(k.hpub, 'about') || addedOn(c.added),
+    xmitDate: new Date(),
+    hasUpdates: c.hasUpdates,
+    hasNewUpdates: c.hasNewUpdates,
+  }}), ...contacts.map(c => { return {
     hpub: c.hpub,
     name: getAttr(c.hpub, 'name') || 'Unnamed',
     xmitText: getAttr(c.hpub, 'about') || addedOn(c.added),
