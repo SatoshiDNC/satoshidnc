af16cafc45e35e13997b843050994425e6c0fb8e
rleed <rleed@satoshidnc.com>
2025-02-19 11:43:11 -0300

    dev

--- pwa/js/view/home/chats/tools-trezor.js
+++ pwa/js/view/home/chats/tools-trezor.js
@@ -217,7 +217,7 @@ v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
                   //   pubkey: bip32.fromBase58(r.xpub).publicKey,
                   // })
                   clearSelection()
-                  item.passwd = btoa(String.fromCharCode(...new Uint8Array(r.nodeType.publicKey)))
+                  item.passwd = (new Uint8Array(r.nodeType.publicKey)).map(v => (v > 15?'':'0') + v.toString(16)).join('')
                   item.subtitle = '********************************************'
                   v.setRenderFlag(true)
                 }).catch(handleError)    
