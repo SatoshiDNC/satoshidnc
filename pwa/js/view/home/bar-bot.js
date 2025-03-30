9e41ba66ab004b065a2a0ab8be49b2e854318d06
rleed <rleed@satoshidnc.com>
2025-03-30 10:03:02 -0300

    dev

--- pwa/js/view/home/bar-bot.js
+++ pwa/js/view/home/bar-bot.js
@@ -40,7 +40,8 @@ for (const pane of v.panes) {
   }
 }
 eventTrigger.push(e => {
-  console.log('new status update (?):', e)
+  if (!e) return
+  console.log('new status update:', e)
   const updatePane = v.paneGads.filter(g => g.label == 'Updates')[0]
   if (updatePane.label != v.activeLabel) {
     updatePane.new = true
