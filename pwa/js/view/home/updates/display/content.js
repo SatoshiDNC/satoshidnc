61f7f632545dd42de9cb78125b74911a82dc8d8d
rleed <rleed@satoshidnc.com>
2025-03-30 08:43:30 -0300

    dev

--- pwa/js/view/home/updates/display/content.js
+++ pwa/js/view/home/updates/display/content.js
@@ -69,7 +69,7 @@ v.renderKind1 = function(data) {
   ts = 50/14
   let plaintext = data.content
   if (encryption == 'cc20s10') {
-    let key = data._key
+    let key = data._key && Uint8Array.from(data._key.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)))
     plaintext = new TextDecoder().decode(crypt(0, Uint8Array.from(data.content.match(/.{1,2}/g).map((byte) => parseInt(byte, 16))), key).map(v => v>32 && v<127? v: 63))
   }
   const paragraphs = plaintext.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
