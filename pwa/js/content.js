fefd67ecbe3b76aec0959be6d9ef7d8cd6ef0af4
rleed <rleed@satoshidnc.com>
2025-03-30 08:18:30 -0300

    dev

--- pwa/js/content.js
+++ pwa/js/content.js
@@ -96,6 +96,7 @@ export function aggregateEvent(e) {
             } else if (e.kind == 24) {
               const ids = []
               const todo = e.tags.filter(t => t[0] == 'e')
+              console.log('unlock', todo, e)
               const unlockProcessing = () => {
                 if (todo.length > 0) {
                   const toUnlock = todo.pop()
@@ -106,6 +107,7 @@ export function aggregateEvent(e) {
                   req.onsuccess = () => {
                     if (req.result) { // the event to unlock is in our database
                       if (req.result.data.pubkey == e.pubkey) {
+                        console.log('unlocking')
                         ids.push(toUnlock)
                         req.result.data._key = e.key.content
                         const req = os.put(req.result.data)
