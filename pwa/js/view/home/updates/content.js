8bf9229dbba965e7bb3b3374c062194130cbd4c5
rleed <rleed@satoshidnc.com>
2025-04-01 13:31:31 -0300

    dev

--- pwa/js/view/home/updates/content.js
+++ pwa/js/view/home/updates/content.js
@@ -347,7 +347,7 @@ v.renderFunc = function() {
     let icon = balance > 0? '❤': '💔'
     mat4.identity(m)
     let iconScale = 20/14
-    let d = 4/iconScale
+    let d = 4*iconScale
     mat4.translate(m, m, [v.sw - 32+28, g.y + 67 + index * 200, 0])
     mat4.scale(m, m, [iconScale, iconScale, 1])
     for (let r = 0; r < rank; r++) {
