8da67e39732ab03ed07a7cda010823003ca9959e
rleed <rleed@satoshidnc.com>
2025-04-24 09:07:20 -0300

    dev

---
+++ pwa/js/view/home/updates/channel/bar-bot.js
@@ -0,0 +1,107 @@
+import { drawPill, drawRect } from '../../draw.js'
+import { getKeyboardInput } from '../util.js'
+import { contacts } from '../../contacts.js'
+
+let v, g
+export const barBot = v = new fg.View()
+v.name = Object.keys({barBot}).pop()
+v.bgColor = [0x0b/0xff, 0x14/0xff, 0x1b/0xff, 1]
+v.gadgets.push(g = v.msgGad = new fg.Gadget(v))
+  g.actionFlags = fg.GAF_CLICKABLE
+  g.x = 127, g.y = 12, g.h = 123
+  g.hint = 'Message'
+  g.text = '', g.defaultValue = 'Type something'
+  g.animValue = 0
+  g.focusValue = 0
+  g.focused = false
+  g.clickFunc = function() {
+    const g = this, v = this.viewport
+    g.focused = true
+    v.setRenderFlag(true)
+    g.timerId = setInterval(() => {
+      if (g.focusValue != 1) return
+      getKeyboardInput(g.label, g.text || g.defaultValue, value => {
+        if (value !== undefined) g.text = value
+        g.focused = false
+        v.setRenderFlag(true)
+      })
+      clearInterval(g.timerId), delete g.timerId
+    }, 10)
+  }
+v.gadgets.push(g = v.sendGad = new fg.Gadget(v))
+  g.actionFlags = fg.GAF_CLICKABLE
+  g.y = 12, g.w = 123, g.h = 123
+  g.label = 'Send'
+  g.handler = function(item) {
+    console.log(`send as ${JSON.stringify(item)}`)
+  }
+  g.items = []
+  g.clickFunc = function() {
+    const g = this, v = this.viewport
+    if (!v.msgGad.text) return
+    g.items = contacts.map(c => { return {
+      name: c.name,
+      hpub: c.hpub,
+      handler: g.handler,
+    } })
+    if (fg.getRoot() !== g.target || g.target.easingState() == -1) {
+      g.target?.easeIn?.(g.items)
+    } else {
+      g.target?.easeOut?.()
+    }
+  }
+v.layoutFunc = function() {
+  const v = this
+  let g
+  g = v.msgGad
+  g.w = v.sw - 400 - g.x
+  g.autoHull()
+  g = v.sendGad
+  g.x = v.sw - 135
+  g.autoHull()
+}
+v.renderFunc = function() {
+  const v = this
+  gl.clearColor(...v.bgColor)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  const m = mat4.create()
+
+  let g = v.msgGad
+  let goal = g.focused? 1: 0
+  if (goal != g.focusValue) {
+    g.focusValue = g.focusValue * 0.0 + goal * 1.0
+    if (Math.abs(goal - g.focusValue) < 0.005) {
+      g.focusValue = goal
+    }
+    v.setRenderFlag(true)
+  }
+  const f1 = g.focusValue
+  const f0 = 1 - f1
+
+  let s = 37/14
+  drawPill(v, colors.chatTextBox, 13, 11, v.sw - 13 - 148, 123)
+  mat4.identity(m)
+  mat4.translate(m,m, [131, 92, 0])
+  mat4.scale(m,m, [s, s, 1])
+  let str
+  const max = g.w - 3
+  if (defaultFont.calcWidth(g.text) * s > max) {
+    let l = g.text.length
+    while (defaultFont.calcWidth(g.text.substring(0,l)+'...') * s > max) {
+      l--
+    }
+    str = g.text.substring(0,l)+'...'
+  } else {
+    str = g.text
+  }
+  defaultFont.draw(0,0, str||g.hint, g.text?[1,1,1,1]:colors.chatInfoText, v.mat, m)
+  drawRect(v, colors.accent.map((v,i)=>i==3?f1:v), g.x, g.y+26, 5, 70)
+
+  g = v.sendGad
+  drawPill(v, colors.accent, g.x, g.y, g.w, g.h)
+  mat4.identity(m)
+  mat4.translate(m,m, [g.x + 42, g.y + 83, 0])
+  s = 42/7
+  mat4.scale(m,m, [s, s, 1])
+  iconFont.draw(0,0, '>', v.bgColor, v.mat, m)
+}
---
+++ pwa/js/view/home/updates/channel/bar-top.js
@@ -0,0 +1,121 @@
+import { drawAvatar } from '../../draw.js'
+
+let v, g
+export const barTop = v = new fg.View()
+v.name = Object.keys({barTop}).pop()
+v.designHeight = 147
+v.bgColor = [0.043,0.078,0.106,1]
+v.textColor = [1,1,1,1]
+v.white = [1,1,1,1]
+v.yellow = [1,0.8,0,1]
+v.red = [1,0,0,1]
+v.blue = [0,0,1,1]
+v.VPOS0 = 90
+v.VPOS1 = 68
+v.lastSubtitle = ''
+v.subtitleOpacity = 0
+v.shirtColor = [v.white, v.yellow, v.red, v.blue]
+v.gadgets.push(g = v.menuGad = new fg.Gadget(v))
+  g.actionFlags = fg.GAF_CLICKABLE
+  g.label = ':'
+  g.font = iconFont
+  g.fontSize = 11
+  g.handler = function(item) {
+    cas
+    console.log(`id ${JSON.stringify(item)}`)
+  }
+  g.items = [
+    { id: 1, label: 'Copy id', handler: () => { navigator.clipboard.writeText(v.contact.hpub).catch(e => console.error(e)) } },
+    { id: 2, handler: g.handler, label: 'View contact' },
+    { id: 3, handler: g.handler, label: 'Media, links, and docs' },
+    { id: 4, handler: g.handler, label: 'Search' },
+    { id: 5, handler: g.handler, label: 'Mute notifications' },
+    { id: 6, handler: g.handler, label: 'Disappearing messages' },
+    { id: 7, handler: g.handler, label: 'Wallpaper' },
+    { id: 8, handler: g.handler, label: 'More' },
+  ]
+  g.clickFunc = function() {
+    const g = this, v = this.viewport
+    if (fg.getRoot() !== g.target || g.target.easingState() == -1) {
+      g.target?.easeIn?.(g.items)
+    } else {
+      g.target?.easeOut?.()
+    }
+  }
+v.gadgets.push(g = v.backGad = new fg.Gadget(v))
+  g.actionFlags = fg.GAF_CLICKABLE
+  g.label = '\x08'
+  g.x = 23, g.y = 52
+  g.w = 42, g.h = 42
+  g.font = iconFont
+  g.fontSize = 13
+  g.autoHull()
+  g.clickFunc = function() {
+    const g = this, v = this.viewport
+    g.root.easeOut(g.target)
+  }
+v.layoutFunc = function() {
+  const v = this
+  let g
+  g = v.menuGad
+  g.x = v.sw - 64
+  g.y = 51
+  g.w = 12
+  g.h = 45
+  g.autoHull()
+}
+v.renderFunc = function() {
+  const v = this, contact = v.contact
+  gl.clearColor(...v.bgColor)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  const m = mat4.create()  
+
+  // subtle divider line
+  mainShapes.useProg2()
+  gl.uniform4fv(gl.getUniformLocation(prog2, 'overallColor'), new Float32Array(colors.inactiveDark))
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uProjectionMatrix'), false, v.mat)
+  mat4.identity(m)
+  mat4.translate(m,m, [0, v.sh-2, 0])
+  mat4.scale(m,m, [v.sw, 2, 1])
+  gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
+  mainShapes.drawArrays2('rect')
+
+  drawAvatar(v, contact.hpub, 75, 27, 92, 92)
+
+  const subtitle = ''
+  v.lastSubtitle ||= subtitle
+  let goal = subtitle? 1: 0
+  if (goal != v.subtitleOpacity) {
+    v.subtitleOpacity = v.subtitleOpacity * 0.7 + goal * 0.3
+    if (Math.abs(goal - v.subtitleOpacity) < 0.005) {
+      v.subtitleOpacity = goal
+    }
+    v.setRenderFlag(true)
+  }
+  const f1 = v.subtitleOpacity
+  const f0 = 1 - f1
+
+  const mat = mat4.create()  
+  mat4.identity(mat)
+  mat4.translate(mat, mat, [190, v.VPOS0 * f0 + v.VPOS1 * f1, 0])
+  mat4.scale(mat, mat, [35/14, 35/14, 1])
+  let x = 0, y = 0
+  defaultFont.draw(x,y, contact.name, v.textColor, v.mat, mat)
+  
+  mat4.identity(mat)
+  mat4.translate(mat, mat, [190, 116, 0])
+  mat4.scale(mat, mat, [23/14, 23/14, 1])
+  x = 0, y = 0
+  // online
+  // last seen today at 12:50 PM
+  // Business Account
+  const c = v.textColor
+  defaultFont.draw(x,y, 'online', [c[0], c[1], c[2], f1], v.mat, mat)
+
+  for (g of v.gadgets) {
+    mat4.identity(mat)
+    mat4.translate(mat, mat, [g.x, g.y+g.h, 0])
+    mat4.scale(mat, mat, [g.h/g.fontSize, g.h/g.fontSize, 1])
+    g.font.draw(0,0, g.label, v.textColor, v.mat, mat)
+  }
+}
