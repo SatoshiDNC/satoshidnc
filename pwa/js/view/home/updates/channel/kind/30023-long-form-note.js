c6b3bcd74dcf437b00d68a533ba7a7fc0fbb48a6
rleed <rleed@satoshidnc.com>
2025-04-24 11:48:15 -0300

    dev

--- pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
+++ pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
@@ -2,64 +2,74 @@ import { crypt } from '../../../../../cryption.js'
 
 let debug = false
 
-export function render_kind30023(view, data) {
-  const v = view
-  const encryption = data.tags.filter(t => t[0] == 'encryption')?.[0]?.[1] || ''
-  let plaintext = data.tags.filter(t => t[0] == 'summary')[0][1]
+export function render_kind30023(view, post) {
+  const v = view, p = post
+  const encryption = p.preloaded.data.tags.filter(t => t[0] == 'encryption')?.[0]?.[1] || ''
+  let plaintext = p.preloaded.data.tags.filter(t => t[0] == 'summary')[0][1]
   // if (encryption == 'cc20s10') {
   //   let key = data._key && Uint8Array.from(data._key.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)))
   //   plaintext = new TextDecoder().decode(crypt(0, Uint8Array.from(data.content.match(/.{1,2}/g).map((byte) => parseInt(byte, 16))), key).map(v => key? v: v>32 && v<127? v: 63))
   // }
 
   const m = mat4.create()
-  const whitespace = false
-  let t,tw,th,ts
-  ts = 50/14
-  const paragraphs = plaintext.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
-  //const paragraphs = ['A😊B']
-  if (debug) paragraphs.pop()
-  if (debug) paragraphs.push('This is a test of the emergency broadcasting system. This is only a test.')
-  const lines = []
-  for (const para of paragraphs) {
-    if (debug) console.log(`for (const para ${para} of paragraphs ${paragraphs}) {`)
-    const words = para.split(' ')
-    if (debug) console.log(`while (words.length ${words.length} > 0) {`)
-    while (words.length > 0) {
-      lines.push(words.shift())
-      if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
-      while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= v.sw) {
-        let buf = ''
+  const ts = TEXT_HEIGHT/14
+
+  // re-calculate geometry on first re-draw
+  if (!p.lines) {
+    let lines = []
+
+    const plaintext = p.preloaded.data.content
+    const whitespace = false
+    const paragraphs = plaintext.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
+    const max_width = v.sw - SPACE_LEFT - SPACE_RIGHT - TEXT_SPACE_LEFT - TEXT_SPACE_RIGHT
+
+    for (const para of paragraphs) {
+      if (debug) console.log(`for (const para ${para} of paragraphs ${paragraphs}) {`)
+      const words = para.split(' ')
+      if (debug) console.log(`while (words.length ${words.length} > 0) {`)
+      while (words.length > 0) {
+        lines.push(words.shift())
         if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
-        while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= v.sw) {
+        while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
           let l = lines.pop()
-          buf = l.substring(l.length-1) + buf
-          l = l.substring(0, l.length-1)
-          lines.push(l)
+          let i = 1, fit = 1
+          while (defaultFont.calcWidth(l.substring(0,i)) * ts <= max_width) {
+            fit = i
+            i += 1
+          }
+          lines.push(l.substring(0, fit))
+          lines.push(l.substring(fit))
+
+
+          // let buf = ''
+          // if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
+          // while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
+          //   let l = lines.pop()
+          //   buf = l.substring(l.length-1) + buf
+          //   l = l.substring(0, l.length-1)
+          //   lines.push(l)
+          // }
+          // lines.push(buf)
         }
-        lines.push(buf)
-      }
-      if (debug) console.log(`while (words.length ${words.length} > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) ${defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0])} * ts ${ts} <= v.sw ${v.sw}) {`)
-      while (words.length > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) * ts <= v.sw) {
-        lines.push(lines.pop() + ' ' + words.shift())
+        if (debug) console.log(`while (words.length ${words.length} > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) ${defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0])} * ts ${ts} <= v.sw ${v.sw}) {`)
+        while (words.length > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) * ts <= max_width) {
+          lines.push(lines.pop() + ' ' + words.shift())
+        }
       }
     }
+    p.lines = lines
   }
-  debug = false
-  // tw = lines.reduce((a,c) => Math.max(a, defaultFont.calcWidth(c) * ts, 0))
-  th = lines.length * defaultFont.glyphHeights[65] * ts * 2
-  let i = 1
-  for (let line of lines) {
-    i++
-    if (!line) continue
-    if (whitespace) {
-      line = line.replaceAll(' ', '·')
-    }
-    tw = defaultFont.calcWidth(line) * ts
+
+  let total_height = TEXT_SPACE_BELOW + TEXT_HEIGHT + (p.lines.length - 1) * TEXT_LINE_SPACING + TEXT_SPACE_ABOVE
+  drawRoundedRect(v, v.bubbleColor, BUBBLE_RADIUS, SPACE_LEFT,v.sh-y-SPACE_BELOW-total_height, v.sw-SPACE_LEFT-SPACE_RIGHT,total_height)
+
+  let line_offset = p.lines.length
+  for (const line of p.lines) {
+    line_offset -= 1
     mat4.identity(m)
-    mat4.translate(m, m, [(v.sw - tw)/2, (v.sh - th)/2 + i*defaultFont.glyphHeights[65]*ts*2, 0])
+    mat4.translate(m, m, [SPACE_LEFT+TEXT_SPACE_LEFT, v.sh-y-SPACE_BELOW-TEXT_SPACE_BELOW-line_offset*TEXT_LINE_SPACING, 0])
     mat4.scale(m, m, [ts, ts, 1])
     defaultFont.draw(0,0, line, v.textColor, v.mat, m)
   }
-
 }
 
