f16f1a79da9b13ab63d3beaf08ff385d54884c5e
rleed <rleed@satoshidnc.com>
2025-04-24 12:14:56 -0300

    dev

--- pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
+++ pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
@@ -15,53 +15,48 @@ export function prep_kind30023(view, post) {
   const m = mat4.create()
   const ts = geom.TEXT_SCALE
 
-  // re-calculate geometry on first re-draw
-  if (!p.lines) {
-    let lines = []
+  let lines = []
 
-    const plaintext = p.preloaded.data.content
-    const whitespace = false
-    const paragraphs = plaintext.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
-    const max_width = v.sw - geom.SPACE_LEFT - geom.SPACE_RIGHT - geom.TEXT_SPACE_LEFT - geom.TEXT_SPACE_RIGHT
+  const whitespace = false
+  const paragraphs = plaintext.replaceAll('\x0a', `${whitespace?'¶':''}\x0a`).split('\x0a')
+  const max_width = v.sw - geom.SPACE_LEFT - geom.SPACE_RIGHT - geom.TEXT_SPACE_LEFT - geom.TEXT_SPACE_RIGHT
 
-    for (const para of paragraphs) {
-      if (debug) console.log(`for (const para ${para} of paragraphs ${paragraphs}) {`)
-      const words = para.split(' ')
-      if (debug) console.log(`while (words.length ${words.length} > 0) {`)
-      while (words.length > 0) {
-        lines.push(words.shift())
-        if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
-        while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
-          let l = lines.pop()
-          let i = 1, fit = 1
-          while (defaultFont.calcWidth(l.substring(0,i)) * ts <= max_width) {
-            fit = i
-            i += 1
-          }
-          lines.push(l.substring(0, fit))
-          lines.push(l.substring(fit))
-
-
-          // let buf = ''
-          // if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
-          // while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
-          //   let l = lines.pop()
-          //   buf = l.substring(l.length-1) + buf
-          //   l = l.substring(0, l.length-1)
-          //   lines.push(l)
-          // }
-          // lines.push(buf)
+  for (const para of paragraphs) {
+    if (debug) console.log(`for (const para ${para} of paragraphs ${paragraphs}) {`)
+    const words = para.split(' ')
+    if (debug) console.log(`while (words.length ${words.length} > 0) {`)
+    while (words.length > 0) {
+      lines.push(words.shift())
+      if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
+      while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
+        let l = lines.pop()
+        let i = 1, fit = 1
+        while (defaultFont.calcWidth(l.substring(0,i)) * ts <= max_width) {
+          fit = i
+          i += 1
         }
-        if (debug) console.log(`while (words.length ${words.length} > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) ${defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0])} * ts ${ts} <= v.sw ${v.sw}) {`)
-        while (words.length > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) * ts <= max_width) {
-          lines.push(lines.pop() + ' ' + words.shift())
-        }
+        lines.push(l.substring(0, fit))
+        lines.push(l.substring(fit))
+
+
+        // let buf = ''
+        // if (debug) console.log(`while (lines[lines.length-1] ${lines[lines.length-1]} && defaultFont.calcWidth(lines[lines.length-1]) ${defaultFont.calcWidth(lines[lines.length-1])} * ts ${ts} >= v.sw ${v.sw}) {`)
+        // while (lines[lines.length-1] && defaultFont.calcWidth(lines[lines.length-1]) * ts >= max_width) {
+        //   let l = lines.pop()
+        //   buf = l.substring(l.length-1) + buf
+        //   l = l.substring(0, l.length-1)
+        //   lines.push(l)
+        // }
+        // lines.push(buf)
+      }
+      if (debug) console.log(`while (words.length ${words.length} > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) ${defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0])} * ts ${ts} <= v.sw ${v.sw}) {`)
+      while (words.length > 0 && defaultFont.calcWidth(lines[lines.length-1] + ' ' + words[0]) * ts <= max_width) {
+        lines.push(lines.pop() + ' ' + words.shift())
       }
     }
-    p.lines = lines
-    p.total_height = geom.TEXT_SPACE_BELOW + geom.TEXT_HEIGHT + (p.lines.length - 1) * geom.TEXT_LINE_SPACING + geom.TEXT_SPACE_ABOVE
-    p.type = 'default'
   }
-
+  p.lines = lines
+  p.total_height = geom.TEXT_SPACE_BELOW + geom.TEXT_HEIGHT + (p.lines.length - 1) * geom.TEXT_LINE_SPACING + geom.TEXT_SPACE_ABOVE
+  p.type = 'default'
 }
 
