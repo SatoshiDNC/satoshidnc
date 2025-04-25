e3195ecf9333081f2c02fed6482bf0ebe18065c8
rleed <rleed@satoshidnc.com>
2025-04-25 20:03:25 -0300

    dev

--- pwa/js/view/home/updates/channel/content.js
+++ pwa/js/view/home/updates/channel/content.js
@@ -172,9 +172,9 @@ v.render_default = function(post, y) {
   const max_h = (p.lines.length - p.lines.filter(l => l=='').length/2) * geom.TEXT_LINE_SPACING
   let w = max_w/3
   drawRect(v, v.textColor, geom.SPACE_LEFT+geom.TEXT_SPACE_LEFT,v.userY, w,2*geom.TEXT_SCALE)
-  const baseline_bottom = v.sh-v.userY-y-geom.TEXT_SPACE_BELOW
+  const baseline_bottom = v.sh-(v.userY+y+geom.TEXT_SPACE_BELOW)
   console.log(baseline_bottom)
-  if (baseline_bottom < 0) {
+  if (baseline_bottom > v.sh) {
     const w = max_w / max_h * (-baseline_bottom)
     drawRect(v, v.textColor, v.sw-geom.SPACE_RIGHT-geom.TEXT_SPACE_RIGHT-w,v.userY+v.sh-2*geom.TEXT_SCALE, w,2*geom.TEXT_SCALE)
   }
