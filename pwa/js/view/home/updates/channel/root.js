38705d0dc6ac6f39c83be8a3abcf6644f81e4fa5
rleed <rleed@satoshidnc.com>
2025-04-26 20:54:24 -0300

    dev

--- pwa/js/view/home/updates/channel/root.js
+++ pwa/js/view/home/updates/channel/root.js
@@ -3,14 +3,14 @@ import { barBot } from './bar-bot.js'
 import { contentView } from './content.js'
 import { setEasingParameters } from '../../../util.js'
 
-export const rootView = v = new fg.DualSliceView(null, 'v', 147, 1)
+export const rootView = v = new fg.SliceView(null, 't', 147)
 v.name = Object.keys({rootView}).pop()
 v.designSize = 1080 * 2183
 v.ghostOpacity = 0 // needed?
 v.bgColor = colors.black
 v.a = barTop; barTop.parent = v
 v.b = contentView; contentView.parent = v
-v.c = barBot; barBot.parent = v
+//v.c = barBot; barBot.parent = v
 v.setContext = function(updates, hpub, returnView) {
   barTop.setContext(hpub)
   contentView.setContext(updates, hpub, returnView)
