3c53f89f2313f42ffde3aa0bbb9780e4c7372a25
rleed <rleed@satoshidnc.com>
2025-04-27 13:02:15 -0300

    dev

--- pwa/js/view/home/updates/content.js
+++ pwa/js/view/home/updates/content.js
@@ -375,7 +375,7 @@ v.renderFunc = function() {
     let balance = balances[hpub]?.['sat'] || 0 // Math.round(Math.random() * 3000 - 1500)
     let rank = balance? `${Math.abs(balance)}`.length: 0
     let rankIcon = balance > 0? '💔': '❤'
-    let rankColor = balance > 0? [0,1,0, 1]: [1,0,0, 1]
+    let rankColor = balance > 0? [1,0,0, 1]: [1,0,0, 1]
 
     let textScale = 35/14
 
