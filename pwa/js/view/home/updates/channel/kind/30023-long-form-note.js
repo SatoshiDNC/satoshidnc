7436f4b21dc194241ae3e14cb4958524cfb78989
rleed <rleed@satoshidnc.com>
2025-04-27 21:41:06 -0300

    dev

--- pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
+++ pwa/js/view/home/updates/channel/kind/30023-long-form-note.js
@@ -67,7 +67,7 @@ export function prep_kind30023_preview(view, post) {
   p.lines = lines
   p.total_height = geom.TEXT_SPACE_BELOW + geom.TEXT_HEIGHT + (p.lines.length - 1 - p.lines.filter(l => l=='').length/2) * geom.TEXT_LINE_SPACING + geom.TEXT_SPACE_ABOVE
   p.type = 'default'
-  p.reactions = ['something']
+  //p.reactions = ['something']
 }
 
 export function prep_kind30023(view, post) {
@@ -143,6 +143,6 @@ export function prep_kind30023(view, post) {
   p.lines = lines
   p.total_height = geom.TEXT_SPACE_BELOW + geom.TEXT_HEIGHT + (p.lines.length - 1 - p.lines.filter(l => l=='').length/2) * geom.TEXT_LINE_SPACING + geom.TEXT_SPACE_ABOVE
   p.type = 'default'
-  p.reactions = ['something']
+  //p.reactions = ['something']
 }
 
