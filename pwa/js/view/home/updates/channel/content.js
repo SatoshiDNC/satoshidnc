7b6fbb686715902ca4d2da0b74c6a438bd8d4d34
rleed <rleed@satoshidnc.com>
2025-04-28 21:11:53 -0300

    dev

--- pwa/js/view/home/updates/channel/content.js
+++ pwa/js/view/home/updates/channel/content.js
@@ -23,7 +23,7 @@ v.buttonTextColor = colors.accentButtonText
 v.previous_width = 0
 personalDataTrigger.push(() => {
   console.log(`[${TAG}] detected personal data change`)
-  v.queueLayout()
+  v.parent.queueLayout()
 })
 v.gadgets.push(g = v.screenGad = new fg.Gadget(v))
   g.actionFlags = fg.GAF_CLICKABLE
