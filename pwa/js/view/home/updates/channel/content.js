18f9a6b6ae86bf5335572690844f75d6f9288bff
rleed <rleed@satoshidnc.com>
2025-04-29 12:27:52 -0300

    dev

--- pwa/js/view/home/updates/channel/content.js
+++ pwa/js/view/home/updates/channel/content.js
@@ -288,6 +288,7 @@ v.render_reactions = function(post, y) {
     mat4.translate(m, m, [x+geom.REACTION_SIZE/2, v.sh-y+geom.REACTIONS_HEIGHT/2, 0])
     mat4.scale(m, m, [ts, ts, 1])
     defaultFont.draw(-diameter/2, diameter/2, String.fromCodePoint(defaultFont.glyphCodes[i]), v.textColor, v.mat, m)
+    x += geom.REACTION_SIZE + geom.REACTION_SPACE_BETWEEN
   }
 }
 v.render_debug_info = function(post, y) {
