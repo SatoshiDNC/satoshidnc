3bd5c0f1b49e6eb884f3aa47c912e903a8bf7c19
rleed <rleed@satoshidnc.com>
2025-04-29 11:05:34 -0300

    dev

--- pwa/js/content.js
+++ pwa/js/content.js
@@ -42,14 +42,16 @@ export function setDummyKey(id) {
 export function aggregateEvent(e) {
   return new Promise((resolve, reject) => {
     if (!e || !e.id || !e.sig || !e.pubkey) reject('invalid event')
-    const TAG = 'aggregateEvent'
+    const TAG = 'aggregate'
+    const LOG_AND_REJECT = () => {
+      console.error(`[${TAG}] database error`)
+      reject()
+    }
     const now = Date.now()
     const tr = db.transaction(['events', 'profiles', 'deletions', 'expirations'], 'readwrite', { durability: 'strict' })
     const os = tr.objectStore('events')
     const req = os.index('id').get(e.id)
-    req.onerror = () => {
-      reject()
-    }
+    req.onerror = LOG_AND_REJECT
     req.onsuccess = () => {
       if (req.result) { // this event is already in our database
         //console.log(`[${TAG}] skipping duplicate event`, JSON.stringify(e))
@@ -57,9 +59,7 @@ export function aggregateEvent(e) {
       } else {
         const del_os = tr.objectStore('deletions')
         const req = del_os.get(e.id)
-        req.onerror = () => {
-          reject()
-        }
+        req.onerror = LOG_AND_REJECT
         req.onsuccess = () => {
           if (req.result) { // there's a deletion record in our database
             console.log(`[${TAG}] skipping deleted event`, JSON.stringify(e))
@@ -69,83 +69,11 @@ export function aggregateEvent(e) {
               console.log(`[${TAG}] skipping event of kind ${e.kind} (${kindInfo.filter(i => i.kind == e.kind)?.[0].desc})`/*, JSON.stringify(e)*/)
               resolve()
             } else if (e.kind == 5) {
-              const ids = []
-              const todo = e.tags.filter(t => t[0] == 'e').map(t => t[1])
-              const deletionProcessing = () => {
-                if (todo.length > 0) {
-                  const toDelete = todo.pop()
-                  const req = del_os.put({ id: toDelete, asOf: now, hpub: e.pubkey })
-                  req.onerror = () => {
-                    reject()
-                  }
-                  req.onsuccess = () => {
-                    const req = os.index('id').get(toDelete)
-                    req.onerror = () => {
-                      reject()
-                    }
-                    req.onsuccess = () => {
-                      if (req.result) { // the deleted event is in our database
-                        if (req.result.data.pubkey == e.pubkey) {
-                          ids.push(toDelete)
-                          req = os.delete(toDelete)
-                          req.onerror = () => {
-                            reject()
-                          }
-                          req.onsuccess = () => {
-                            deletionProcessing()
-                          }
-                        } else {
-                          console.log(`warning: deletion attempt by different pubkey: ${e.pubkey} tried to delete post by ${req.result.data.pubkey}`)
-                          deletionProcessing()
-                        }
-                      } else {
-                        deletionProcessing()
-                      }
-                    }
-                  }
-                } else {
-                  console.log(`[${TAG}] noted deletions:`, ids)
-                  setTimeout(() => { deletionTrigger.map(f => f(ids)) })
-                  resolve()
-                }
-              }
-              deletionProcessing()
+              handle_incoming_deletion(e, os, del_os, TAG, LOG_AND_REJECT, resolve, reject)
             } else if (e.kind == 24) {
-              const ids = []
-              const todo = e.tags.filter(t => t[0] == 'e').map(t => t[1])
-              const unlockProcessing = () => {
-                if (todo.length > 0) {
-                  const toUnlock = todo.pop()
-                  const req = os.index('id').get(toUnlock)
-                  req.onerror = () => {
-                    reject()
-                  }
-                  req.onsuccess = () => {
-                    if (req.result) { // the event to unlock is in our database
-                      if (req.result.data.pubkey == e.pubkey) {
-                        ids.push(toUnlock)
-                        req.result.data._key = e.content
-                        const req2 = os.put({ hpub: req.result.hpub, firstSeen: req.result.firstSeen, data: req.result.data })
-                        req2.onerror = () => {
-                          reject()
-                        }
-                        req2.onsuccess = () => {
-                          unlockProcessing()
-                        }
-                      } else {
-                        console.log(`warning: unlock attempt by different pubkey: ${e.pubkey} tried to unlock post by ${req.result.data.pubkey}`)
-                        unlockProcessing()
-                      }
-                    } else {
-                      unlockProcessing()
-                    }
-                  }
-                } else {
-                  setTimeout(() => { unlockTrigger.map(f => f(ids)) })
-                  resolve()
-                }
-              }
-              unlockProcessing()
+              handle_incoming_key(e, os, TAG, LOG_AND_REJECT, resolve, reject)
+            } else if (e.kind == 7) {
+              handle_incoming_reaction(e, tr, TAG, LOG_AND_REJECT, resolve, reject)
             } else {
               console.log(`[${TAG}] new event`, JSON.stringify(e))
               const req = os.add({ hpub: e.pubkey, firstSeen: now, data: e })
@@ -197,9 +125,116 @@ export function aggregateEvent(e) {
   })
 }
 
+function handle_incoming_deletion(e, os, del_os, tag, database_error_handler, resolve, reject) {
+  const ids = []
+  const todo = e.tags.filter(t => t[0] == 'e').map(t => t[1])
+  const deletionProcessing = () => {
+    if (todo.length > 0) {
+      const toDelete = todo.pop()
+      const req = del_os.put({ id: toDelete, asOf: now, hpub: e.pubkey })
+      req.onerror = database_error_handler
+      req.onsuccess = () => {
+        const req = os.index('id').get(toDelete)
+        req.onerror = database_error_handler
+        req.onsuccess = () => {
+          if (req.result) { // the deleted event is in our database
+            if (req.result.data.pubkey != e.pubkey) {
+              console.log(`[${tag}] ignored deletion attempt by different pubkey: ${e.pubkey} tried to delete post by ${req.result.data.pubkey}`)
+              deletionProcessing()
+            } else {
+              ids.push(toDelete)
+              req = os.delete(toDelete)
+              req.onerror = database_error_handler
+              req.onsuccess = () => {
+                deletionProcessing()
+              }
+            }
+          } else {
+            deletionProcessing()
+          }
+        }
+      }
+    } else {
+      console.log(`[${tag}] noted deletions:`, ids)
+      setTimeout(() => { deletionTrigger.map(f => f(ids)) })
+      resolve()
+    }
+  }
+  deletionProcessing()  
+}
+
+function handle_incoming_key(e, os, tag, database_error_handler, resolve, reject) {
+  const ids = []
+  const todo = e.tags.filter(t => t[0] == 'e').map(t => t[1])
+  const unlockProcessing = () => {
+    if (todo.length > 0) {
+      const toUnlock = todo.pop()
+      const req = os.index('id').get(toUnlock)
+      req.onerror = database_error_handler
+      req.onsuccess = () => {
+        if (req.result) { // the event to unlock is in our database
+          if (req.result.data.pubkey != e.pubkey) {
+            console.log(`[${tag}] ignored unlock attempt by different pubkey: ${e.pubkey} tried to unlock post by ${req.result.data.pubkey}`)
+            unlockProcessing()
+          } else {
+            ids.push(toUnlock)
+            req.result.data._key = e.content
+            const req2 = os.put({ hpub: req.result.hpub, firstSeen: req.result.firstSeen, data: req.result.data })
+            req2.onerror = database_error_handler
+            req2.onsuccess = () => {
+              unlockProcessing()
+            }
+          }
+        } else {
+          unlockProcessing()
+        }
+      }
+    } else {
+      console.log(`[${tag}] unlocked posts:`, ids)
+      setTimeout(() => { unlockTrigger.map(f => f(ids)) })
+      resolve()
+    }
+  }
+  unlockProcessing()
+}
+
+function handle_incoming_reaction(e, tr, tag, database_error_handler, resolve, reject) {
+  const os = tr.objectStore('events')
+  const reaction_os = tr.objectStore('incoming-reactions-pending')
+  const event_reacted_to = e.tags.filter(t => t[0] == 'e').map(t => t[1]).pop()
+  if (event_reacted_to) {
+    const req = os.index('id').get(event_reacted_to)
+    req.onerror = database_error_handler
+    req.onsuccess = () => {
+      if (req.result) { // the event reacted to is in our database
+        if (!req.result.data._reactions) { req.result.data._reactions = []}
+        if (!req.result.data._reactions[e.content]) { req.result.data._reactions[e.content] = 0 }
+        req.result.data._reactions[e.content] += 1
+        const req2 = os.put({ hpub: req.result.hpub, firstSeen: req.result.firstSeen, data: req.result.data })
+        req2.onerror = database_error_handler
+        req2.onsuccess = () => {
+          console.log(`[${tag}] reaction on ${event_reacted_to}`)
+          setTimeout(() => { reactionTrigger.map(f => f(event_reacted_to)) })
+          resolve()
+        }
+      } else {
+        const req2 = reaction_os.put({ data: e, target: event_reacted_to })
+        req2.onerror = database_error_handler
+        req2.onsuccess = () => {
+          console.log(`[${tag}] queued reaction to unknown post: ${event_reacted_to}`)
+          resolve()
+        }
+      }
+    }
+  } else {
+    console.warn(`[${tag}] invalid reaction:`, e)
+    reject()
+  }
+}
+
 export function savePendingReactions(reactions) {
-  const tr = db.transaction(['reactions-pending'], 'readwrite', { durability: 'strict' })
-  const os = tr.objectStore('reactions-pending')
+  const tr = db.transaction(['outgoing-reactions-signed'], 'readwrite', { durability: 'strict' })
+  const os = tr.objectStore('outgoing-reactions-signed')
   reactions.map(reaction => {
     const req = os.put({ id: reaction.tags.filter(t => t[0] == 'e')[0][1], data: reaction })
     req.onerror = e => {
@@ -213,8 +248,8 @@ export function savePendingReactions(reactions) {
 
 export function sendPendingReaction(id) {
   const TAG = 'sendPendingReaction'
-  const tr = db.transaction(['reactions-pending'], 'readwrite', { durability: 'strict' })
-  const os = tr.objectStore('reactions-pending')
+  const tr = db.transaction(['outgoing-reactions-signed'], 'readwrite', { durability: 'strict' })
+  const os = tr.objectStore('outgoing-reactions-signed')
   const req = os.get(id)
   req.onerror = e => {
     console.error(`database error`)
--- pwa/js/db.js
+++ pwa/js/db.js
@@ -2,6 +2,10 @@ const TAG = 'db'
 
 export let db
 
+export const log_and_reject = () => {
+  console.error(`[${TAG}] database error`)
+}
+
 export function init() {
   return new Promise((resolve, reject) => {
     const req = ((typeof window !== 'undefined')? window.indexedDB : indexedDB).open('db', 8)
@@ -20,43 +24,51 @@ export function init() {
       }
       let os
       if (e.oldVersion < 1) {
-        db.createObjectStore(`keys`, { keyPath: 'hpub' })
-        db.createObjectStore(`contacts`, { keyPath: 'hpub' })
-        db.createObjectStore(`relays`, { keyPath: 'url' })
-        db.createObjectStore(`relay-contact-relations`, { keyPath: ['relayUrl', 'contactHpub', 'relation'] })
-        db.createObjectStore(`personal`, { keyPath: ['hpub', 'key'] })
+        db.createObjectStore('keys', { keyPath: 'hpub' })
+        db.createObjectStore('contacts', { keyPath: 'hpub' })
+        db.createObjectStore('relays', { keyPath: 'url' })
+        db.createObjectStore('relay-contact-relations', { keyPath: ['relayUrl', 'contactHpub', 'relation'] })
+        db.createObjectStore('personal', { keyPath: ['hpub', 'key'] })
       }
       if (e.oldVersion < 2) {
-        db.createObjectStore(`relay-stats`, { keyPath: ['relayUrl', 'key'] })
-        os = db.createObjectStore(`events`, { keyPath: 'data.id' })
-        os.createIndex(`id`, 'data.id')
-        os.createIndex(`createdAt`, 'data.created_at')
-        os.createIndex(`firstSeen`, 'firstSeen')
-        os.createIndex(`hpub_firstSeen`, ['hpub', 'firstSeen'])
-        db.createObjectStore(`updates-viewed`, { keyPath: 'id' })
+        db.createObjectStore('relay-stats', { keyPath: ['relayUrl', 'key'] })
+        os = db.createObjectStore('events', { keyPath: 'data.id' })
+        os.createIndex('id', 'data.id')
+        os.createIndex('createdAt', 'data.created_at')
+        os.createIndex('firstSeen', 'firstSeen')
+        os.createIndex('hpub_firstSeen', ['hpub', 'firstSeen'])
+        db.createObjectStore('updates-viewed', { keyPath: 'id' })
       }
       if (e.oldVersion < 3) {
-        db.createObjectStore(`profiles`, { keyPath: 'hpub' })
-        db.createObjectStore(`deletions`, { keyPath: 'id' })
+        db.createObjectStore('profiles', { keyPath: 'hpub' })
+        db.createObjectStore('deletions', { keyPath: 'id' })
       }
       if (e.oldVersion < 4) {
-        os = db.createObjectStore(`expirations`, { keyPath: 'id' })
-        os.createIndex(`expiry`, 'expiry')
+        os = db.createObjectStore('expirations', { keyPath: 'id' })
+        os.createIndex('expiry', 'expiry')
       }
       if (e.oldVersion < 5) {
-        os = db.createObjectStore(`updates-new`, { keyPath: 'hpub' })
+        os = db.createObjectStore('updates-new', { keyPath: 'hpub' })
       }
       if (e.oldVersion < 6) {
-        os = db.createObjectStore(`deals-pending`, { keyPath: 'data.id' })
-        os.createIndex(`id`, 'data.id')
+        os = db.createObjectStore('deals-pending', { keyPath: 'data.id' })
+        os.createIndex('id', 'data.id')
       }
       if (e.oldVersion < 7) {
-        os = db.createObjectStore(`reactions-pending`, { keyPath: 'id' })
-        os.createIndex(`id`, 'id')
+        os = db.createObjectStore('reactions-pending', { keyPath: 'id' })
+        os.createIndex('id', 'id')
       }
       if (e.oldVersion < 8) {
-        os = db.createObjectStore(`channels-followed`, { keyPath: 'hpub' })
-        os.createIndex(`hpub`, 'hpub')
+        os = db.createObjectStore('channels-followed', { keyPath: 'hpub' })
+        os.createIndex('hpub', 'hpub')
+      }
+      if (e.oldVersion < 9) {
+        os = db.createObjectStore('incoming-reactions-pending', { keyPath: 'data.id' })
+        os.createIndex('target', 'target')
+
+        db.deleteObjectStore('reactions-pending')
+        os = db.createObjectStore('outgoing-reactions-signed', { keyPath: 'id' })
+        os.createIndex('id', 'id')
       }
     }
   })
