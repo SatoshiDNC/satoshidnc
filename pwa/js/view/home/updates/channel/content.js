99e6d678221147b19c9d6887b2a5c351fd2a4328
rleed <rleed@satoshidnc.com>
2025-04-29 13:07:02 -0300

    dev

--- pwa/js/view/home/updates/channel/content.js
+++ pwa/js/view/home/updates/channel/content.js
@@ -277,9 +277,6 @@ v.renderFunc = function() {
 v.render_reactions = function(post, y) {
   const v = this, p = post
   const m = mat4.create()
-  drawPill(v, v.bgColor, geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT,v.sh-y,200,geom.REACTIONS_HEIGHT)
-  drawPill(v, v.bubbleColor, geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT+geom.REACTIONS_BORDER,v.sh-y+geom.REACTIONS_BORDER,200-2*geom.REACTIONS_BORDER,geom.REACTIONS_HEIGHT-2*geom.REACTIONS_BORDER)
-  let x = geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT+geom.REACTIONS_BORDER+geom.REACTION_SPACE_LEFT
 
   const groups = {}
   for (const reaction of Object.keys(p.reactions)) {
@@ -302,7 +299,18 @@ v.render_reactions = function(post, y) {
       ranking.splice(0, 0, group)
     }
   }
-  
+
+  let w = 0
+  for (const reaction of ranking) {
+    let i = +reaction // defaultFont.glyphCodes.indexOf(reaction.codePointAt(0))
+    if (i == -1) { i = defaultFont.glyphCodes.indexOf('?'.codePointAt(0)) }
+    const magnitude = 1 + ((''+groups[reaction]).length-1)/15
+    w += geom.REACTION_SIZE*magnitude + geom.REACTION_SPACE_BETWEEN
+  }
+
+  drawPill(v, v.bgColor, geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT,v.sh-y,w,geom.REACTIONS_HEIGHT)
+  drawPill(v, v.bubbleColor, geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT+geom.REACTIONS_BORDER,v.sh-y+geom.REACTIONS_BORDER,w-2*geom.REACTIONS_BORDER,geom.REACTIONS_HEIGHT-2*geom.REACTIONS_BORDER)
+  let x = geom.SPACE_LEFT+geom.REACTIONS_SPACE_LEFT+geom.REACTIONS_BORDER+geom.REACTION_SPACE_LEFT
   for (const reaction of ranking) {
     let i = +reaction // defaultFont.glyphCodes.indexOf(reaction.codePointAt(0))
     if (i == -1) { i = defaultFont.glyphCodes.indexOf('?'.codePointAt(0)) }
