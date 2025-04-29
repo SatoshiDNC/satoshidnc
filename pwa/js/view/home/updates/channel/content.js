0a3a4c1b82eb6e388438aed06419d5bdcf448ee4
rleed <rleed@satoshidnc.com>
2025-04-29 13:10:22 -0300

    dev

--- pwa/js/view/home/updates/channel/content.js
+++ pwa/js/view/home/updates/channel/content.js
@@ -300,7 +300,7 @@ v.render_reactions = function(post, y) {
     }
   }
 
-  let w = geom.REACTION_SPACE_LEFT+geom.REACTION_SPACE_RIGHT
+  let w = geom.REACTION_SPACE_LEFT-geom.REACTION_SPACE_BETWEEN+geom.REACTION_SPACE_RIGHT
   for (const reaction of ranking) {
     let i = +reaction // defaultFont.glyphCodes.indexOf(reaction.codePointAt(0))
     if (i == -1) { i = defaultFont.glyphCodes.indexOf('?'.codePointAt(0)) }
