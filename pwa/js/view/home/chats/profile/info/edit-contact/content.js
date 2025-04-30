785bcbb0ab1286afe87bfad17b1a3c8887ebbdbf
rleed <rleed@satoshidnc.com>
2025-04-30 18:47:12 -0300

    dev

--- pwa/js/view/home/chats/profile/info/edit-contact/content.js
+++ pwa/js/view/home/chats/profile/info/edit-contact/content.js
@@ -26,6 +26,8 @@ v.gadgets.push(g = v.nameGad = new fg.Gadget(v))
     v.setRenderFlag(true)
     g.timerId = setInterval(() => {
       if (g.focusValue != 1) return
+      const gad = g
+      const g = gad
       getKeyboardInput(g.label, g.text || g.defaultValue, value => {
         if (value !== undefined) {
           g.text = value
