0ac5771a884e5ae6c9cea22a26236de8e2c2e6ca
rleed <rleed@satoshidnc.com>
2025-04-30 17:23:35 -0300

    dev

--- pwa/js/draw.js
+++ pwa/js/draw.js
@@ -102,7 +102,7 @@ export function drawAvatar(v, hpub, x,y,w,h, hearts) {
     mat4.identity(m)
     mat4.translate(m, m, [x + w/2, y + h/2, 0])
     mat4.scale(m, m, [w/diameter, h/diameter, 1])
-    defaultFont.draw(-defaultFont.glyphX1[i], defaultFont.glyphHeights[i]-defaultFont.glyphY1[i], String.fromCodePoint(i), color, v.mat, m)
+    defaultFont.draw(-defaultFont.glyphX1[i]-defaultFont.glyphWidths[i]/2, defaultFont.glyphHeights[i]/2-defaultFont.glyphY1[i], String.fromCodePoint(i), color, v.mat, m)
   }
 
   if (mode == 'hpub') {
