4ed01bb61ef59d8d15ad1174bca1a2caccf66424
rleed <rleed@satoshidnc.com>
2025-05-06 10:35:57 -0300

    dev

--- pwa/js/sw/periodic.js
+++ pwa/js/sw/periodic.js
@@ -2,7 +2,4 @@ const TAG = 'sw-minutely'
 
 export function minutelyTasks() {
   console.log(`[${TAG}] no-op`)
-  self.clients.matchAll().then(list => {
-    list[0]?.postMessage({ note: 'this is a test'})
-  })
 }
